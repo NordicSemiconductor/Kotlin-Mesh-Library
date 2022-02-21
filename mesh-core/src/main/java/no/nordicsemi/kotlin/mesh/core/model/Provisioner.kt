97504f9b490d781a5abfa710fccc735d56b678be
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2022-02-21 10:41:53 +0100

    Provisioner model

---
+++ mesh-core/src/main/java/no/nordicsemi/kotlin/mesh/core/model/Provisioner.kt
@@ -0,0 +1,23 @@
+package no.nordicsemi.kotlin.mesh.core.model
+
+import kotlinx.serialization.SerialName
+import java.util.*
+
+/**
+ * A Provisioner is a mesh node that is capable of provisioning a device to the mesh network and,
+ * is represented by a provisioner object in the Mesh Configuration Database
+ *
+ * @param name                      Provisioner name.
+ * @param uuid                      UUID of the provisioner.
+ * @param allocatedUnicastRanges    List of allocated unicast ranges for a given provisioner.
+ * @param allocatedGroupRanges      List of allocated group ranges for a given provisioner.
+ * @param allocatedSceneRanges      List of allocated scene ranges for a given provisioner.
+ */
+data class Provisioner(
+    @SerialName("provisionerName")
+    val name: String,
+    val uuid: UUID,
+    val allocatedUnicastRanges: List<AllocatedUnicastRange>,
+    val allocatedGroupRanges: List<AllocatedGroupRange>,
+    val allocatedSceneRanges: List<AllocatedSceneRange>
+)
