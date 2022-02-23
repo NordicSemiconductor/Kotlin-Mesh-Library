1bebd42a4930d795448d08ad9330b769456564bc
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2022-02-23 12:14:14 +0100

    Publish model

---
+++ mesh-core/src/main/java/no/nordicsemi/kotlin/mesh/core/model/Publish.kt
@@ -0,0 +1,32 @@
+@file:Suppress("unused")
+
+package no.nordicsemi.kotlin.mesh.core.model
+
+import java.util.*
+
+/**
+ * The publish object represents parameters that define how the messages are published by a mesh model.
+ *
+ * @param address       The address property contains the publication [MeshAddress] for the model containing a [Address].
+ * @param index         The index property contains an integer that represents an [ApplicationKey] index, indicating which
+                        Application Key to use for the publication. The application key index corresponds to the index value
+                        of one of the application key entries in the node’s appKeys array.
+ * @param ttl           The ttl property contains an integer from 0 to 127 that represents the Time to Live (TTL) value for
+                        published messages or an integer with a value of 255 that indicates that the node’s default TTL is used
+                        for publication.
+ * @param period        The period property refers to [PublishPeriod] that describes the interval between subsequent publications.
+                        If the value of this property is 0, the periodic publication is disabled.
+ * @param credentials   The [Credentials] property contains an integer of 0 or 1 that represents whether managed flooding security
+                        material (0) or friendship security material (1) is used.
+ * @param retransmit    The [Retransmit] property describes the number of times a message is published and the interval between
+ *                      retransmissions of the published messages.
+ */
+data class Publish(
+    val address: MeshAddress,
+    var label: UUID,
+    val index: Int,
+    val ttl: Int,
+    val period: PublishPeriod,
+    val credentials: Int,
+    val retransmit: Retransmit
+)
