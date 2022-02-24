8198d5f9b74ac90b999700786e18e94e6c7c3f40
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2022-02-24 09:40:48 +0100

    Wrapper class for Model ID

---
+++ mesh-core/src/main/java/no/nordicsemi/kotlin/mesh/core/model/ModelId.kt
@@ -0,0 +1,21 @@
+package no.nordicsemi.kotlin.mesh.core.model
+
+/**
+ * Wrapper class for 16-bit or 32-bit model identifier.
+ */
+sealed class ModelId(internal val modelId: UInt)
+
+/**
+ * Wrapper class for 16-bit Bluetooth SIG model identifier.
+ *
+ * @param modelIdentifier 16-bit model identifier.
+ */
+data class SigModelId(val modelIdentifier: UShort) : ModelId(modelId = modelIdentifier.toUInt())
+
+/**
+ * Wrapper class for 32-bit vendor model identifier.
+ *
+ * @param modelIdentifier 32-bit model identifier.
+ */
+data class VendorModelId(val modelIdentifier: UShort, val companyIdentifier: UShort) :
+    ModelId(modelId = (modelIdentifier.toUInt()) or (companyIdentifier.toUInt() shl 16))
