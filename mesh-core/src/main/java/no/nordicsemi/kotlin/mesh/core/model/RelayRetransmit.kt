649ad4eccfad344ce675e3ea01dcf44d08acbfba
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2022-02-25 12:37:12 +0100

    Refactor retransmit model

--- mesh-core/src/main/java/no/nordicsemi/kotlin/mesh/core/model/RelayRetransmit.kt
+++ mesh-core/src/main/java/no/nordicsemi/kotlin/mesh/core/model/RelayRetransmit.kt
@@ -7,4 +7,4 @@ package no.nordicsemi.kotlin.mesh.core.model
  * @param count       An integer from 0 to 7 that represents the number of retransmissions for published messages. A value of 0 represents no retransmissions.
  * @param interval    The interval property contains an integer from 50 to 1600, with a resolution of 50, that represents the interval in milliseconds between the transmissions.
  */
-data class Retransmit(val count: Int, val interval: Int)
+data class RelayRetransmit(val count: Int, val interval: Int)
