5bf9c064816ecdbd710606f7e84b4f7dd83c02d0
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2022-03-01 21:19:05 +0100

    Refactor mesh model and fix documentation.

--- mesh-core/src/main/java/no/nordicsemi/kotlin/mesh/core/model/Model.kt
+++ mesh-core/src/main/java/no/nordicsemi/kotlin/mesh/core/model/Model.kt
@@ -3,39 +3,17 @@ package no.nordicsemi.kotlin.mesh.core.model
 /**
  * Represents a configured state of a mesh model.
  *
- * @param modelId       The [ModelId] property contains a 16-bit [SigModelId] that represents a Bluetooth SIG defined model
+ * @property modelId       The [ModelId] property contains a 16-bit [SigModelId] that represents a Bluetooth SIG defined model
  *                      identifier field or a 32-bit [VendorModelId] that represents a vendor-defined model identifier.
- * @param subscribe     The subscribe property contains an array [MeshAddress].
- * @param publish       The publish property contains a [Publish] that describes the configuration of this model’s publication.
- * @param bind          The bind property contains an array of integers that represents indexes of the [ApplicationKey] to which
+ * @property subscribe     The subscribe property contains a list of [MeshAddress].
+ * @property publish       The publish property contains a [Publish] that describes the configuration of this model’s publication.
+ * @property bind          The bind property contains a list of integers that represents indexes of the [ApplicationKey] to which
  *                      this model is bound. Each application key index corresponds to the index values of one of the application
- *                      key entries in the node’s [ApplicationKey] array.
+ *                      key entries in the node’s [ApplicationKey] list.
  */
 data class Model internal constructor(
     val modelId: ModelId,
-    internal val subscribe: Array<MeshAddress>,
+    internal val subscribe: List<MeshAddress>,
     val publish: Publish,
-    internal val bind: Array<Int>,
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Model
-
-        if (modelId != other.modelId) return false
-        if (!subscribe.contentEquals(other.subscribe)) return false
-        if (publish != other.publish) return false
-        if (!bind.contentEquals(other.bind)) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = modelId.hashCode()
-        result = 31 * result + subscribe.contentHashCode()
-        result = 31 * result + publish.hashCode()
-        result = 31 * result + bind.contentHashCode()
-        return result
-    }
-}
+    internal val bind: List<Int>,
+)
--- mesh-core/src/main/java/no/nordicsemi/kotlin/mesh/core/model/ModelId.kt
+++ mesh-core/src/main/java/no/nordicsemi/kotlin/mesh/core/model/ModelId.kt
@@ -1,3 +1,5 @@
+@file:Suppress("unused")
+
 package no.nordicsemi.kotlin.mesh.core.model
 
 /**
@@ -8,14 +10,14 @@ sealed class ModelId(internal val modelId: UInt)
 /**
  * Wrapper class for 16-bit Bluetooth SIG model identifier.
  *
- * @param modelIdentifier 16-bit model identifier.
+ * @property modelIdentifier 16-bit model identifier.
  */
 data class SigModelId(val modelIdentifier: UShort) : ModelId(modelId = modelIdentifier.toUInt())
 
 /**
  * Wrapper class for 32-bit vendor model identifier.
  *
- * @param modelIdentifier 32-bit model identifier.
+ * @property modelIdentifier 32-bit model identifier.
  */
 data class VendorModelId(val modelIdentifier: UShort, val companyIdentifier: UShort) :
     ModelId(modelId = (modelIdentifier.toUInt()) or (companyIdentifier.toUInt() shl 16))
