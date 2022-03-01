5ea1710b0d29207b66ca8e7d9a37adc3fc2ad43a
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2022-03-01 21:24:10 +0100

    Fix documentation.

--- mesh-core/src/main/java/no/nordicsemi/kotlin/mesh/core/model/NetworkTransmit.kt
+++ mesh-core/src/main/java/no/nordicsemi/kotlin/mesh/core/model/NetworkTransmit.kt
@@ -4,12 +4,24 @@ package no.nordicsemi.kotlin.mesh.core.model
  * The network transmit object represents the parameters of the transmissions of network layer messages
  * originating from a mesh node.
  *
- * @param count 	The count property contains an integer from 1 to 8 that represents the number of transmissions for
- * 					network messages.
- * @param interval	The interval property contains an integer from 10 to 320 that represents the interval in milliseconds
- * 					between the transmissions.
+ * @param count         The count property contains an integer from 1 to 8 that represents the number of transmissions for
+ * 					    network messages.
+ * @param interval      The interval property contains an integer from 10 to 320 that represents the interval in milliseconds
+ * 					    between the transmissions.
  */
 data class NetworkTransmit internal constructor(
     val count: Int,
     val interval: Int
-)
+) {
+    init {
+        require(count in MIN_COUNT..MAX_COUNT) { "Count must be a value from $MIN_COUNT to $MAX_COUNT number of transmissions!" }
+        require(interval in MIN_INTERVAL..MAX_INTERVAL) { "Interval must be a value from $MIN_INTERVAL to $MAX_INTERVAL milliseconds between transmissions!" }
+    }
+
+    companion object {
+        const val MIN_COUNT = 1
+        const val MAX_COUNT = 8
+        const val MIN_INTERVAL = 10
+        const val MAX_INTERVAL = 320
+    }
+}
--- mesh-core/src/main/java/no/nordicsemi/kotlin/mesh/core/model/RelayRetransmit.kt
+++ mesh-core/src/main/java/no/nordicsemi/kotlin/mesh/core/model/RelayRetransmit.kt
@@ -4,7 +4,7 @@ package no.nordicsemi.kotlin.mesh.core.model
  * The retransmit object is used to describe the number of times a message is published and the interval
  * between retransmissions of the published messages.
  *
- * @param count       An integer from 0 to 7 that represents the number of retransmissions for published messages. A value of 0 represents no retransmissions.
- * @param interval    The interval property contains an integer from 50 to 1600, with a resolution of 50, that represents the interval in milliseconds between the transmissions.
+ * @property count       An integer from 0 to 7 that represents the number of retransmissions for published messages. A value of 0 represents no retransmissions.
+ * @property interval    The interval property contains an integer from 50 to 1600, with a resolution of 50, that represents the interval in milliseconds between the transmissions.
  */
-data class RelayRetransmit(val count: Int, val interval: Int)
+data class RelayRetransmit internal constructor(val count: Int, val interval: Int)
