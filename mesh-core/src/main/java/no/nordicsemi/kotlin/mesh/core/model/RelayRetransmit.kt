f0adb1eadc545893042c27c03e92c3f7060fe167
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2022-04-01 14:17:37 +0200

    Improve relay retransmit.

--- mesh-core/src/main/java/no/nordicsemi/kotlin/mesh/core/model/RelayRetransmit.kt
+++ mesh-core/src/main/java/no/nordicsemi/kotlin/mesh/core/model/RelayRetransmit.kt
@@ -3,16 +3,29 @@ package no.nordicsemi.kotlin.mesh.core.model
 import kotlinx.serialization.Serializable
 
 /**
- * The retransmit object is used to describe the number of times a message is published and the interval
- * between retransmissions of the published messages.
+ * The relay retransmit object represents the parameters of the retransmissions of network layer
+ * messages relayed by a mesh node.
  *
- * @property count       An integer from 1 to 8 that represents the number of transmissions for relay messages
- * @property interval    an integer from 10 to 320 that represents the interval in milliseconds between the transmissions.
+ * @property count       An integer from 1 to 8 that represents the number of transmissions for
+ *                       relay messages
+ * @property interval    an integer from 10 to 320 that represents the interval in milliseconds
+ *                       between the transmissions.
  */
 @Serializable
 data class RelayRetransmit internal constructor(val count: Int, val interval: Int) {
     init {
-        require(count in 1..8) {"Count must be a from 1 to 8"}
-        require(interval in 10..320) {"Interval must range from 10 to 320"}
+        require(count in MIN_COUNT..MAX_COUNT) {
+            "Count must be a from $MIN_COUNT to $MAX_COUNT"
+        }
+        require(interval in MIN_INTERVAL..MAX_INTERVAL) {
+            "Interval must be from $MIN_INTERVAL to $MAX_INTERVAL"
+        }
+    }
+
+    private companion object {
+        const val MIN_COUNT = 1
+        const val MAX_COUNT = 8
+        const val MIN_INTERVAL = 10
+        const val MAX_INTERVAL = 320
     }
 }
