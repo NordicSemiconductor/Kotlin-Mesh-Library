c73260ac9304b4187378c576840f3c3fa3960d90
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2022-04-04 14:53:49 +0200

    Update timestamp when group is updated.

--- mesh-core/src/main/java/no/nordicsemi/kotlin/mesh/core/model/Group.kt
+++ mesh-core/src/main/java/no/nordicsemi/kotlin/mesh/core/model/Group.kt
@@ -4,19 +4,21 @@ package no.nordicsemi.kotlin.mesh.core.model
 
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
+import kotlin.properties.Delegates
 
 /**
  * Group defines a [GroupAddress] of type [PrimaryGroupAddress] to which a node may subscribe to.
  *
- * If a node is part of a Group, at least one model of the node is subscribed to the Group’s group address.
- * A Group may have a Parent Group. In this case, all the models of a node that are subscribed to the
- * Group’s address are also subscribed to the Parent Group’s address.
+ * If a node is part of a Group, at least one model of the node is subscribed to the Group’s group
+ * address. A Group may have a Parent Group. In this case, all the models of a node that are
+ * subscribed to the Group’s address are also subscribed to the Parent Group’s address.
  *
  * For example, the Second-Floor Group is a parent of the Bedroom Group and the Guest Bedroom Group.
- * In this case, at least one model of all the nodes of the Bedroom Group is subscribed to a group address
- * or virtual label of the Bedroom Group and Second-Floor Group; and at least one model of all the nodes of
- * the Guest Bedroom Group is subscribed to the group address or virtual label of the Guest Bedroom
- * Group and the Second-Floor Group. Note library does not validate for cyclic parent-groups.
+ * In this case, at least one model of all the nodes of the Bedroom Group is subscribed to a group
+ * address or virtual label of the Bedroom Group and Second-Floor Group; and at least one model of
+ * all the nodes of the Guest Bedroom Group is subscribed to the group address or virtual label of
+ * the Guest Bedroom Group and the Second-Floor Group. Note library does not validate for cyclic
+ * parent-groups.
  *
  * @property name             Group name.
  * @property address          Address of the group.
@@ -24,16 +26,31 @@ import kotlinx.serialization.Transient
  */
 @Serializable
 data class Group(
-    val name: String,
     val address: PrimaryGroupAddress,
 ) {
-    var parentAddress: ParentGroupAddress = UnassignedAddress
+    var name: String by Delegates.observable(
+        initialValue = "nRF Group"
+    ) { _, oldValue, newValue ->
+        require(newValue.isNotBlank()) {
+            "Group name cannot be empty!"
+        }
+        MeshNetwork.onChange(
+            oldValue = oldValue,
+            newValue = newValue,
+            action = { network?.updateTimestamp() })
+    }
+    var parentAddress: ParentGroupAddress by Delegates.observable(
+        initialValue = UnassignedAddress
+    ) { _, oldValue, newValue ->
+        require(address.address != parentAddress.address) {
+            "Primary group address cannot be the same as the parent group address!"
+        }
+        MeshNetwork.onChange(
+            oldValue = oldValue,
+            newValue = newValue,
+            action = { network?.updateTimestamp() })
+    }
 
     @Transient
     internal var network: MeshNetwork? = null
-
-    init {
-        require(name.isNotBlank()) { "Group name cannot be blank!" }
-        require(address.address != parentAddress.address) { "Primary group address cannot be the same as the parent group address!" }
-    }
 }
