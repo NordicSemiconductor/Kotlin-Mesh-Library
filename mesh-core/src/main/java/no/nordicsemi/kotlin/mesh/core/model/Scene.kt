b715161e2c70a294b4eaaf4a08035abe913dc802
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2022-04-04 14:45:01 +0200

    Update timestamp when scene updated.

--- mesh-core/src/main/java/no/nordicsemi/kotlin/mesh/core/model/Scene.kt
+++ mesh-core/src/main/java/no/nordicsemi/kotlin/mesh/core/model/Scene.kt
@@ -4,6 +4,7 @@ package no.nordicsemi.kotlin.mesh.core.model
 
 import kotlinx.serialization.Serializable
 import no.nordicsemi.kotlin.mesh.core.model.serialization.UShortAsStringSerializer
+import kotlin.properties.Delegates
 
 typealias SceneNumber = UShort
 
@@ -15,13 +16,26 @@ typealias SceneNumber = UShort
  * @property addresses     Addresses containing the scene.
  */
 @Serializable
-data class Scene(
-    val name: String,
+data class Scene internal constructor(
     @Serializable(with = UShortAsStringSerializer::class)
     val number: SceneNumber,
 ) {
+    var name: String by Delegates.observable(
+        initialValue = "nRF Scene"
+    ) { _, oldValue, newValue ->
+        require(newValue.isNotBlank()) {
+            "Scene name empty!"
+        }
+        MeshNetwork.onChange(
+            oldValue = oldValue,
+            newValue = newValue,
+            action = { network?.updateTimestamp() })
+    }
     var addresses = listOf<UnicastAddress>()
         private set
+
+    var network: MeshNetwork? = null
+        internal set
 
     init {
         require(name.isNotBlank()) { "Scene name cannot be blank!" }
@@ -38,6 +52,7 @@ data class Scene(
         addresses.contains(address) -> false
         else -> {
             addresses = addresses + address
+            network?.updateTimestamp()
             true
         }
     }
@@ -49,6 +64,7 @@ data class Scene(
      */
     fun add(addresses: List<UnicastAddress>) {
         this.addresses = this.addresses.union(addresses).toList()
+        network?.updateTimestamp()
     }
 
     companion object {
