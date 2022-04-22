ed7584f8cb7ecd8c97b1e502925a36e2d7178bdd
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2022-04-22 16:01:24 +0200

    Clean up

--- mesh-core/src/main/java/no/nordicsemi/kotlin/mesh/core/exceptions/MeshNetworkExceptions.kt
+++ mesh-core/src/main/java/no/nordicsemi/kotlin/mesh/core/exceptions/MeshNetworkExceptions.kt
@@ -2,57 +2,38 @@ package no.nordicsemi.kotlin.mesh.core.exceptions
 
 sealed class MeshNetworkExceptions : Exception()
 
-/**
- * Thrown when a given key index is out of range. A valid key index must range from 0 to 4095.
- */
+/** Thrown when a given key index is out of range. A valid key index must range from 0 to 4095. */
 class KeyIndexOutOfRange : MeshNetworkExceptions()
 
-/**
- * Thrown when a given key index is already in use.
- */
+/** Thrown when a given key index is already in use. */
 class DuplicateKeyIndex : MeshNetworkExceptions()
 
-/**
- * Thrown when a given key is in use.
- */
+/** Thrown when a given key is in use. */
 class KeyInUse : MeshNetworkExceptions()
 
-/**
- * Thrown when no unicast address range is allocated to a provisioner.
- */
+/** Thrown when no unicast address range is allocated to a provisioner. */
+class ProvisionerAlreadyExists : MeshNetworkExceptions()
+
+/** Thrown when no unicast address range is allocated to a provisioner. */
 class NoUnicastRangeAllocated : MeshNetworkExceptions()
 
-/**
- * Thrown when a given group already exists.
- */
+/** Thrown when a given group already exists. */
 class GroupAlreadyExists : MeshNetworkExceptions()
 
-/**
- * Thrown when a given group is in use.
- */
+/** Thrown when a given group is in use. */
 class GroupInUse : MeshNetworkExceptions()
 
-/**
- * Thrown when no group range is allocated to a provisioner.
- */
+/** Thrown when no group range is allocated to a provisioner. */
 class NoGroupRangeAllocated : MeshNetworkExceptions()
 
-/**
- * Thrown when a given scene already exists.
- */
+/** Thrown when a given scene already exists. */
 class SceneAlreadyExists : MeshNetworkExceptions()
 
-/**
- * Thrown when a given scene is in use.
- */
+/** Thrown when a given scene is in use. */
 class SceneInUse : MeshNetworkExceptions()
 
-/**
- * Thrown when an object does not belong to the current network.
- */
-class DoesNotBelongToNetwork : MeshNetworkExceptions()
-
-/**
- * Thrown when no scene range is allocated to a provisioner.
- */
+/** Thrown when no scene range is allocated to a provisioner. */
 class NoSceneRangeAllocated : MeshNetworkExceptions()
+
+/** Thrown when an object does not belong to the current network. */
+class DoesNotBelongToNetwork : MeshNetworkExceptions()
