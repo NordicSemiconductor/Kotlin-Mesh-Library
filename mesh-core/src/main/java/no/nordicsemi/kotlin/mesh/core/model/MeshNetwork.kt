72972581dfa775d2b027e0b17e066a5ac73ce895
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2022-05-18 13:20:41 +0200

    Improve API

--- mesh-core/src/main/java/no/nordicsemi/kotlin/mesh/core/model/MeshNetwork.kt
+++ mesh-core/src/main/java/no/nordicsemi/kotlin/mesh/core/model/MeshNetwork.kt
@@ -399,49 +399,49 @@ class MeshNetwork internal constructor(
     }
 
     /**
-     *  Check if the given range is allocatable to the given provisioner.
+     *  Checks if the given range is allocatable to the given provisioner.
      *
-     *  @param range    Ranges to be allocated.
-     *  @param uuid     UUID of the provisioner.
+     *  @param range           Ranges to be allocated.
+     *  @param provisioner     Provisioner to which, range must be allocatable.
      *  @return true if the range is not in use by another provisioner or false otherwise.
      *  @throws DoesNotBelongToNetwork if the provisioner does not belong to the network.
      */
     @Throws(DoesNotBelongToNetwork::class)
-    fun isRangeAllocated(range: Range, uuid: UUID) = when (range) {
+    fun isRangeAvailableForAllocation(range: Range, provisioner: Provisioner) = when (range) {
         is UnicastRange ->
             provisioners
-                .filter { it.uuid != uuid }
+                .filter { it.uuid != provisioner.uuid }
                 .none { it._allocatedUnicastRanges.overlaps(range) }
         is GroupRange ->
             provisioners
-                .filter { it.uuid != uuid }
+                .filter { it.uuid != provisioner.uuid }
                 .none { it._allocatedGroupRanges.overlaps(range) }
         is SceneRange ->
             provisioners
-                .filter { it.uuid != uuid }
+                .filter { it.uuid != provisioner.uuid }
                 .none { it._allocatedSceneRanges.overlaps(range) }
     }
 
     /**
      *  Check if the given list of ranges are allocatable to a given provisioner.
      *
-     *  @param ranges   Ranges to be allocated.
-     *  @param uuid     UUID of the provisioner.
+     *  @param ranges          Ranges to be allocated.
+     *  @param provisioner     Provisioner to which, ranges must be allocatable.
      *  @return true if the given ranges are not in use by another provisioner or false otherwise.
      */
-    fun areRangesAllocated(ranges: List<Range>, uuid: UUID) = try {
+    fun areRangesAvailableForAllocation(ranges: List<Range>, provisioner: Provisioner) = try {
         when (ranges.first()) {
             is UnicastRange ->
                 provisioners
-                    .filter { it.uuid != uuid }
+                    .filter { it.uuid != provisioner.uuid }
                     .none { it._allocatedUnicastRanges.overlaps(ranges) }
             is GroupRange ->
                 provisioners
-                    .filter { it.uuid != uuid }
+                    .filter { it.uuid != provisioner.uuid }
                     .none { it._allocatedGroupRanges.overlaps(ranges) }
             is SceneRange ->
                 provisioners
-                    .filter { it.uuid != uuid }
+                    .filter { it.uuid != provisioner.uuid }
                     .none { it._allocatedSceneRanges.overlaps(ranges) }
         }
     } catch (e: NoSuchElementException) {
