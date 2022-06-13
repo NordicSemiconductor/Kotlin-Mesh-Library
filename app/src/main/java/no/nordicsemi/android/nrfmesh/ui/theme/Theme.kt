534959f51f72baa3ac2a5e4e11a4ab8cd4cc7f73
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2022-06-13 09:30:10 +0200

    Fix theming

--- app/src/main/java/no/nordicsemi/android/nrfmesh/ui/theme/Theme.kt
+++ app/src/main/java/no/nordicsemi/android/nrfmesh/ui/theme/Theme.kt
@@ -5,30 +5,38 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = NordicBlue,
+    primaryVariant = NordicBlueDark,
+    secondary = NordicLake,
+    background = Color.Black,
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onSurface = Color.White
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
+    primary = NordicBlue,
+    primaryVariant = NordicBlueDark,
+    secondary = NordicLake,
+    secondaryVariant = NordicBlueSlate,
+    background = NordicBackground,
     surface = Color.White,
     onPrimary = Color.White,
-    onSecondary = Color.Black,
+    onSecondary = Color.White,
     onBackground = Color.Black,
     onSurface = Color.Black,
-    */
 )
 
 @Composable
-fun NRFMeshTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun NordicTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
@@ -41,4 +49,17 @@ fun NRFMeshTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composabl
         shapes = Shapes,
         content = content
     )
+
+    /**
+     * There was a problem with navigation bar color on Samsung 8 with Android 9.
+     * Because Dark Theme was introduced in Android 10, which may be possible cause of the problem,
+     * the navigation bar color is set programmatically for older versions of Android.
+     */
+    if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.Q) {
+        val systemUiController = rememberSystemUiController()
+
+        SideEffect {
+            systemUiController.setNavigationBarColor(color = colors.background)
+        }
+    }
 }
