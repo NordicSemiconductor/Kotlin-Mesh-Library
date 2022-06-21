ec1458e9da4dcdca01cea57e122dd7e5ca08973e
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2022-06-21 09:44:15 +0200

    WIP: Initial UI

--- app/src/main/java/no/nordicsemi/android/nrfmesh/ui/NetworksScreen.kt
+++ app/src/main/java/no/nordicsemi/android/nrfmesh/ui/NetworksScreen.kt
@@ -3,10 +3,14 @@ package no.nordicsemi.android.nrfmesh.ui
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SmallTopAppBar
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.Preview
+import no.nordicsemi.android.nrfmesh.core.ui.MeshButton
 import no.nordicsemi.android.nrfmesh.viewmodel.NetworksViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -29,7 +33,11 @@ fun NetworksScreen(viewModel: NetworksViewModel, modifier: Modifier = Modifier) 
 
 @Composable
 fun ImportNetwork() {
-    Button(onClick = {}) {
-        Text(modifier = Modifier.padding(all = 8.dp), text = "Import Network")
-    }
+    MeshButton(text = "Import Network", onClick = {})
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    ImportNetwork()
 }
