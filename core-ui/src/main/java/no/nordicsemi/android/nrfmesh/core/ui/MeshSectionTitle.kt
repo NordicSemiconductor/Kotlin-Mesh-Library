e5f44fd1343203916efb8b101f64e42c8b608794
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2022-07-01 13:49:47 +0200

    Adds reusable composable MeshSectionTitle

---
+++ core-ui/src/main/java/no/nordicsemi/android/nrfmesh/core/ui/MeshSectionTitle.kt
@@ -0,0 +1,18 @@
+package no.nordicsemi.android.nrfmesh.core.ui
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SectionTitle(title: String, style: TextStyle = MaterialTheme.typography.labelLarge) {
+    Text(
+        modifier = Modifier.padding(start = 16.dp, top = 16.dp, bottom = 8.dp),
+        text = title,
+        style = style
+    )
+}
