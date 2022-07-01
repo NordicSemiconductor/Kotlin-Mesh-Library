ebcb3b4516df8b90ebf2b1e9074b054c070945a5
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2022-07-01 20:50:22 +0200

    Adds support to adding 0x prefix for hex values.

--- mesh-crypto/src/main/java/no/nordicsemi/kotlin/mesh/crypto/Utils.kt
+++ mesh-crypto/src/main/java/no/nordicsemi/kotlin/mesh/crypto/Utils.kt
@@ -5,14 +5,19 @@ import java.util.*
 object Utils {
     private val HEX_CHARS = "0123456789abcdef".toCharArray()
 
-    fun ByteArray.encodeHex(): String {
+    fun ByteArray.encodeHex(prefixOx: Boolean = false): String {
         val hex = CharArray(2 * this.size)
         this.forEachIndexed { i, byte ->
             val unsigned = 0xff and byte.toInt()
             hex[2 * i] = HEX_CHARS[unsigned / 16]
             hex[2 * i + 1] = HEX_CHARS[unsigned % 16]
         }
-        return hex.joinToString("").uppercase(Locale.US)
+        return hex.joinToString("").uppercase(Locale.US).let {
+            when (prefixOx) {
+                true -> "0x$it"
+                false -> it
+            }
+        }
     }
 
     fun String.decodeHex(): ByteArray {
