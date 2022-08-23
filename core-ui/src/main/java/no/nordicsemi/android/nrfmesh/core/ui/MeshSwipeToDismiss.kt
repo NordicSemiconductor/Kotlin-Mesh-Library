66aff47c3da7f3084b68256302c66b67929ee625
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2022-08-23 14:38:02 +0200

    Adds support for SwipeToDismiss

---
+++ core-ui/src/main/java/no/nordicsemi/android/nrfmesh/core/ui/MeshSwipeToDismiss.kt
@@ -0,0 +1,46 @@
+@file:OptIn(ExperimentalMaterialApi::class)
+
+package no.nordicsemi.android.nrfmesh.core.ui
+
+import androidx.compose.animation.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
+
+/**
+ * Holds the Swipe to dismiss composable, its animation and the current state
+ */
+@Composable
+fun SwipeDismissItem(
+    modifier: Modifier = Modifier,
+    dismissState: DismissState = rememberDismissState(),
+    directions: Set<DismissDirection> = setOf(DismissDirection.EndToStart),
+    enter: EnterTransition = expandVertically(),
+    exit: ExitTransition = shrinkVertically(),
+    background: @Composable (offset: Dp) -> Unit,
+    content: @Composable (isDismissed: Boolean) -> Unit
+) {
+    /*// Hold the current state from the Swipe to Dismiss composable
+    val dismissState = rememberDismissState()*/
+    // Boolean value used for hiding the item if the current state is dismissed
+    val isDismissed = dismissState.isDismissed(DismissDirection.EndToStart)
+    // Returns the swiped value in dp
+    val offset = with(LocalDensity.current) { dismissState.offset.value.toDp() }
+
+    AnimatedVisibility(
+        modifier = modifier,
+        visible = !isDismissed,
+        enter = enter,
+        exit = exit
+    ) {
+        SwipeToDismiss(
+            modifier = modifier,
+            state = dismissState,
+            directions = directions,
+            background = { background(offset) },
+            dismissContent = { content(isDismissed) }
+        )
+    }
+}
