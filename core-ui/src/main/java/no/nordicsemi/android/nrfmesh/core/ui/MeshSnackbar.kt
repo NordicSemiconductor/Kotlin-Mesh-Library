f288fb72410457f65e3c7553dcb56ca629c5cc44
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2022-09-15 16:07:17 +0200

    Dismiss already showing snackbar.

--- core-ui/src/main/java/no/nordicsemi/android/nrfmesh/core/ui/MeshSnackbar.kt
+++ core-ui/src/main/java/no/nordicsemi/android/nrfmesh/core/ui/MeshSnackbar.kt
@@ -95,7 +95,7 @@ fun showSnackbar(
     scope.launch {
         // Let's dismiss any snackbar that's been shown already.
         // https://developer.android.com/reference/kotlin/androidx/compose/material3/package-summary#Snackbar(androidx.compose.ui.Modifier,kotlin.Function0,kotlin.Function0,kotlin.Boolean,androidx.compose.ui.graphics.Shape,androidx.compose.ui.graphics.Color,androidx.compose.ui.graphics.Color,androidx.compose.ui.graphics.Color,androidx.compose.ui.graphics.Color,kotlin.Function0)
-        // snackbarHostState.currentSnackbarData?.dismiss()
+        snackbarHostState.currentSnackbarData?.dismiss()
         when (snackbarHostState.showSnackbar(message, null, withDismissAction, duration)) {
             SnackbarResult.Dismissed -> onDismissed()
             else -> {}
