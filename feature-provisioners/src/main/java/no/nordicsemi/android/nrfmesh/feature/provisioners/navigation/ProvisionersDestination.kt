296280795f0c68284d9ff6d382a8d4972fa379dc
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2022-09-20 15:18:24 +0200

    Add feature-provisioner module.

---
+++ feature-provisioners/src/main/java/no/nordicsemi/android/nrfmesh/feature/provisioners/navigation/ProvisionersDestination.kt
@@ -0,0 +1,27 @@
+@file:Suppress("unused")
+
+package no.nordicsemi.android.nrfmesh.feature.provisioners.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import no.nordicsemi.android.nrfmesh.core.navigation.MeshNavigationDestination
+import no.nordicsemi.android.nrfmesh.feature.provisioners.ProvisionersRoute
+import java.util.*
+
+object ScenesDestination : MeshNavigationDestination {
+    override val route: String = "provisioners_route"
+    override val destination: String = "provisioners_destination"
+}
+
+fun NavGraphBuilder.provisionersGraph(
+    onBackPressed: () -> Unit,
+    onNavigateToProvisioner: (UUID) -> Unit
+) {
+    composable(route = ProvisionerDestination.route) {
+        ProvisionersRoute(
+            navigateToProvisioner = onNavigateToProvisioner,
+            onBackClicked = onBackPressed
+        )
+    }
+    provisionerGraph(onBackPressed = onBackPressed)
+}
