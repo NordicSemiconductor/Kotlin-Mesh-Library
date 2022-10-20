c1d21f0d9a99aca0e836c88dec9c0202c6b6303c
Paul-Andrei Petan <pap36@bath.ac.uk>
2022-10-20 14:44:24 +0200

    Simplified The Network Manager
    - save should be called independently from create or import
    - expose the mesh network (useful to get the uuid for the storage when saving) and remove the need of a shared flow

--- mesh-core/src/main/java/no/nordicsemi/kotlin/mesh/core/MeshNetworkManager.kt
+++ mesh-core/src/main/java/no/nordicsemi/kotlin/mesh/core/MeshNetworkManager.kt
@@ -2,12 +2,7 @@
 
 package no.nordicsemi.kotlin.mesh.core
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.*
-import kotlinx.serialization.json.JsonObject
 import no.nordicsemi.kotlin.mesh.core.exception.ImportError
 import no.nordicsemi.kotlin.mesh.core.model.MeshNetwork
 import no.nordicsemi.kotlin.mesh.core.model.serialization.MeshNetworkSerializer.deserialize
@@ -21,44 +16,25 @@ import java.util.*
  * @param storage Custom storage option allowing users to save the mesh network to a custom
  *                location.
  */
-class MeshNetworkManager(private val storage: LocalStorage) {
-    private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
-    internal lateinit var meshNetwork: MeshNetwork
-
-    private val _network = MutableSharedFlow<MeshNetwork>(
-        replay = 1,
-        extraBufferCapacity = 10,
-        onBufferOverflow = BufferOverflow.DROP_OLDEST
-    )
-    val network: SharedFlow<MeshNetwork> = _network.asSharedFlow()
-
-    init {
-        storage.dataStream.onEach {
-            meshNetwork = if (it.isNotEmpty()) deserialize(it)
-            else create()
-            _network.emit(meshNetwork)
-        }.launchIn(scope)
-    }
-
+class MeshNetworkManager(private val storage: Storage) {
+    var meshNetwork: MeshNetwork? = null
+        private set
 
     /**
      * Loads the network from the storage provided by the user.
      *
      * @return true if the configuration was successfully loaded or false otherwise.
      */
-    suspend fun load() = storage.dataStream.first().let {
-        meshNetwork = when (it.isNotEmpty()) {
-            true -> deserialize(it)
-            false -> create()
-        }
+    suspend fun load() = storage.load()?.let {
+        meshNetwork = deserialize(it)
         true
-    }
+    } ?: false
 
     /**
      * Saves the network in the local storage provided by the user.
      */
     suspend fun save() {
-        storage.save(uuid = meshNetwork.uuid, network = exportNetwork().toString())
+        export()?.also { storage.save(it) }
     }
 
     /**
@@ -69,11 +45,8 @@ class MeshNetworkManager(private val storage: LocalStorage) {
      * @param uuid 128-bit Universally Unique Identifier (UUID), which allows differentiation among
      *             multiple mesh networks.
      */
-    suspend fun create(name: String = "Mesh Network", uuid: UUID = UUID.randomUUID()): MeshNetwork {
-        return MeshNetwork(uuid = uuid, _name = name).also {
-            meshNetwork = it
-        }.also { save() }
-    }
+    suspend fun create(name: String = "Mesh Network", uuid: UUID = UUID.randomUUID()) =
+        MeshNetwork(uuid = uuid, _name = name).also { meshNetwork = it }
 
     /**
      * Imports a Mesh Network from a byte array containing a Json defined by the Mesh Configuration
@@ -83,13 +56,8 @@ class MeshNetworkManager(private val storage: LocalStorage) {
      * @throws ImportError if deserializing fails.
      */
     @Throws(ImportError::class)
-    suspend fun import(array: ByteArray): MeshNetwork = run {
-        deserialize(array).also {
-            //_network.emit(it)
-            meshNetwork = it
-            save()
-        }
-    }
+    suspend fun import(array: ByteArray) =
+        deserialize(array).also { meshNetwork = it }
 
     /**
      * Exports a mesh network to a Json defined by the Mesh Configuration Database Profile based
@@ -100,27 +68,11 @@ class MeshNetworkManager(private val storage: LocalStorage) {
      */
     suspend fun export(
         configuration: NetworkConfiguration = NetworkConfiguration.Full
-    ): ByteArray {
-        return exportNetwork(
+    ) = meshNetwork?.let {
+        serialize(
+            network = it,
             configuration = configuration
         ).toString().toByteArray()
-    }
-
-    /**
-     * Internal api that Exports a mesh network to a Json defined by the Mesh Configuration Database
-     * Profile based on the given configuration.
-     *
-     * @param configuration Specifies if the network should be fully exported or partially.
-     * @return JsonObject containing the mesh network configuration.
-     */
-    private fun exportNetwork(
-        configuration: NetworkConfiguration = NetworkConfiguration.Full
-    ): JsonObject {
-        val network = meshNetwork
-        return serialize(
-            network = network,
-            configuration = configuration
-        )
     }
 }
 
