8035aec40c6656f175ac7ecc38d4eb8b3810531e
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2023-03-10 12:42:41 +0100

    Adds support for provisioning algorithms

---
+++ mesh-provisioning/src/main/java/no/nordicsemi/android/mesh/provisioning/Algorithm.kt
@@ -0,0 +1,36 @@
+@file:Suppress("ClassName", "unused")
+
+package no.nordicsemi.android.mesh.provisioning
+
+sealed class Algorithm(val rawValue: UShort) {
+    private constructor(rawValue: Int) : this(rawValue.toUShort())
+
+    object FIPS_P256_ELLIPTIC_CURVE : Algorithm(rawValue = 1 shl 0)
+    object BTM_ECDH_P256_CMAC_AES128_AES_CCM : Algorithm(rawValue = 1 shl 0)
+    object BTM_ECDH_P256_HMAC_SHA256_AES_CCM : Algorithm(rawValue = 1 shl 1)
+
+    internal companion object {
+        /**
+         * Returns the name of the given algorithm.
+         *
+         * @param algorithm The algorithm to get the name of.
+         */
+        fun name(algorithm: Algorithm): String = when (algorithm) {
+            FIPS_P256_ELLIPTIC_CURVE, BTM_ECDH_P256_CMAC_AES128_AES_CCM ->
+                "BTM ECDH P256 CMAC AES128 AES CCM"
+            BTM_ECDH_P256_HMAC_SHA256_AES_CCM ->
+                "BTM ECDH P256 HMAC SHA256 AES CCM"
+        }
+
+        /**
+         * Returns the algorithm from the given provisioning pdu.
+         *
+         * @param pdu The provisioning pdu.
+         */
+        fun from(pdu: ProvisioningPdu): Algorithm? = when (pdu[1]) {
+            0x00.toByte() -> BTM_ECDH_P256_CMAC_AES128_AES_CCM
+            0x01.toByte() -> BTM_ECDH_P256_HMAC_SHA256_AES_CCM
+            else -> null
+        }
+    }
+}
