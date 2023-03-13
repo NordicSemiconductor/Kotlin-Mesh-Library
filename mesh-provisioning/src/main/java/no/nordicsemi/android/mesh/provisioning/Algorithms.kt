7f75e88b5653b0a82acf00d3fe952552d4e398e3
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2023-03-13 16:02:22 +0100

    Improvements to supported Algorithms

---
+++ mesh-provisioning/src/main/java/no/nordicsemi/android/mesh/provisioning/Algorithms.kt
@@ -0,0 +1,86 @@
+@file:Suppress("ClassName", "unused", "MemberVisibilityCanBePrivate")
+
+package no.nordicsemi.android.mesh.provisioning
+
+/**
+ * Algorithm used for calculating a Device Key.
+ *
+ * @property length        Length of the algorithm.
+ * @property value         Value of the algorithm.
+ */
+enum class Algorithm {
+
+    /**
+     * `FIPS_P256_ELLIPTIC_CURVE` algorithm will be used to calculate the shared secret.
+     * This has been renamed to `BTM_ECDH_P256_CMAC_AES128_AES_CCM` in Mesh Protocol 1.1.
+     */
+    @Deprecated(
+        message = "Renamed to BTM_ECDH_P256_CMAC_AES128_AES_CCM in Mesh Protocol 1.1",
+        replaceWith = ReplaceWith("BTM_ECDH_P256_CMAC_AES128_AES_CCM"),
+        level = DeprecationLevel.WARNING
+    )
+    FIPS_P256_ELLIPTIC_CURVE,
+
+    /**
+     * `BTM_ECDH_P256_CMAC_AES128_AES_CCM` algorithm will be used to calculate the shared secret.
+     */
+    BTM_ECDH_P256_CMAC_AES128_AES_CCM,
+
+    /**
+     * `BTM_ECDH_P256_HMAC_SHA256_AES_CCM` algorithm will be used to calculate the shared secret.
+     */
+    BTM_ECDH_P256_HMAC_SHA256_AES_CCM;
+
+    val length: Int
+        get() = when (this) {
+            FIPS_P256_ELLIPTIC_CURVE, BTM_ECDH_P256_CMAC_AES128_AES_CCM -> 128
+            BTM_ECDH_P256_HMAC_SHA256_AES_CCM -> 256
+        }
+
+    val value: UByte
+        get() = when (this) {
+            FIPS_P256_ELLIPTIC_CURVE, BTM_ECDH_P256_CMAC_AES128_AES_CCM -> 0x00u
+            BTM_ECDH_P256_HMAC_SHA256_AES_CCM -> 0x01u
+        }
+
+    internal companion object {
+        /**
+         * Returns the algorithm from the given provisioning pdu.
+         *
+         * @param pdu The provisioning pdu.
+         */
+        fun from(pdu: ProvisioningPdu): Algorithm? = when (pdu[1]) {
+            0x00.toByte() -> BTM_ECDH_P256_CMAC_AES128_AES_CCM
+            0x01.toByte() -> BTM_ECDH_P256_HMAC_SHA256_AES_CCM
+            else -> null
+        }
+    }
+}
+
+/**
+ * A set of algorithms supported by the unprovisioned device.
+ *
+ * @property rawValue The raw value of the algorithm.
+ */
+sealed class Algorithms(private val rawValue: UShort) {
+    private constructor(rawValue: Int) : this(rawValue.toUShort())
+
+    @Deprecated(
+        message = "Renamed to BTM_ECDH_P256_CMAC_AES128_AES_CCM in Mesh Protocol 1.1",
+        replaceWith = ReplaceWith("BTM_ECDH_P256_CMAC_AES128_AES_CCM"),
+        level = DeprecationLevel.WARNING
+    )
+    object FIPS_P256_ELLIPTIC_CURVE : Algorithms(rawValue = 1 shl 0)
+    object BTM_ECDH_P256_CMAC_AES128_AES_CCM : Algorithms(rawValue = 1 shl 0)
+    object BTM_ECDH_P256_HMAC_SHA256_AES_CCM : Algorithms(rawValue = 1 shl 1)
+
+    /**
+     * Returns the name of the given algorithm.
+     */
+    fun name(): String = when (this) {
+        FIPS_P256_ELLIPTIC_CURVE, BTM_ECDH_P256_CMAC_AES128_AES_CCM ->
+            "BTM ECDH P256 CMAC AES128 AES CCM"
+        BTM_ECDH_P256_HMAC_SHA256_AES_CCM ->
+            "BTM ECDH P256 HMAC SHA256 AES CCM"
+    }
+}
