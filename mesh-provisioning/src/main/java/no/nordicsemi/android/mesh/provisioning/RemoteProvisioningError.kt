0ea04dafe244ac52bae235c45719c2a007e6eac6
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2023-03-13 15:06:23 +0100

    Implements a set of errors reported by the an unprovisioned device during provisioning.

---
+++ mesh-provisioning/src/main/java/no/nordicsemi/android/mesh/provisioning/RemoteProvisioningError.kt
@@ -0,0 +1,56 @@
+@file:Suppress("unused")
+
+package no.nordicsemi.android.mesh.provisioning
+
+
+/**
+ * Set of errors which may be reported by an unprovisioned device during provisioning process.
+ */
+enum class RemoteProvisioningError(val errorCode: Int) {
+
+    /**
+     * The provisioning protocol PDU is not recognized by the device.
+     */
+    INVALID_PDU(errorCode = 1),
+
+    /**
+     * The arguments of the protocol PDUs are outside expected values or the length of the PDU is
+     * different than expected.
+     */
+    INVALID_FORMAT(errorCode = 2),
+
+    /**
+     * The PDU received was not expected at this moment of the procedure.
+     */
+    UNEXPECTED_PDU(errorCode = 3),
+
+    /**
+     * The computed confirmation value was not successfully verified.
+     */
+    CONFIRMATION_FAILED(errorCode = 4),
+
+    /**
+     * The provisioning protocol cannot be continued due to insufficient resources in the device.
+     */
+    OUT_OF_RESOURCES(errorCode = 5),
+
+    /**
+     * The Data block was not successfully decrypted.
+     */
+    DECRYPTION_FAILED(errorCode = 6),
+
+    /**
+     * An unexpected error occurred that may not be recoverable.
+     */
+    UNEXPECTED_ERROR(errorCode = 7),
+
+    /**
+     * The device cannot assign consecutive unicast addresses to all elements.
+     */
+    CANNOT_ASSIGN_ADDRESS(errorCode = 8),
+
+    /**
+     * The Data block contains values that cannot be accepted because of general constraints.
+     */
+    INVALID_ADDRESS(errorCode = 9);
+}
