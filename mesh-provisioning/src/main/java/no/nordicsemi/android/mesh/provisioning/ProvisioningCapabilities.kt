db1cd287e3fc532672b5546c522a26cce0236ec6
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2023-03-13 15:53:47 +0100

    Adds support for ProvisioningCapabilities PDU

---
+++ mesh-provisioning/src/main/java/no/nordicsemi/android/mesh/provisioning/ProvisioningCapabilities.kt
@@ -0,0 +1,25 @@
+package no.nordicsemi.android.mesh.provisioning
+
+/**
+ * The device sends this PDU to indicate the supported capabilities to a provisioner.
+ *
+ * @property numberOfElements                 Number of elements supported by the device.
+ * @property algorithms                       Algorithms supported by the device.
+ * @property publicKeyType                    Public key type supported by the device.
+ * @property oobType                          OOB type supported by the device.
+ * @property outputOobSize                    Output OOB size supported by the device.
+ * @property outputOobActions                 Output OOB actions supported by the device.
+ * @property inputOobSize                     Input OOB size supported by the device.
+ * @property inputOobActions                  Input OOB actions supported by the device.
+ * @constructor Creates a [ProvisioningCapabilities] object.
+ */
+data class ProvisioningCapabilities(
+    val numberOfElements: Int,
+    val algorithms: Algorithms,
+    val publicKeyType: PublicKeyType,
+    val oobType: OobType,
+    val outputOobSize: UByte,
+    val outputOobActions: OutputOobActions,
+    val inputOobSize: UByte,
+    val inputOobActions: InputOobActions
+)
