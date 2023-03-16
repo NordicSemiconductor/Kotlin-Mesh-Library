0b65cab52ae5643b6fe128928bc1d4c13cba24e6
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2023-03-16 15:17:20 +0100

    Adds support for Transmitter that will send messages and the respective bearer over which the messages are sent.

---
+++ mesh-core/src/main/java/no/nordicsemi/kotlin/mesh/core/bearer/Bearer.kt
@@ -0,0 +1,49 @@
+@file:Suppress("unused")
+
+package no.nordicsemi.kotlin.mesh.core.bearer
+
+import no.nordicsemi.kotlin.mesh.core.exception.InvalidPduType
+
+
+/**
+ * A transmitter is responsible for delivering messages to the mesh network.
+ */
+interface Transmitter {
+
+    /**
+     * Sends the given data over the bearer. Data longer than MTU will automatically be segmented
+     * using the bearer protocol if bearer implements segmentation.
+     *
+     * @param pdu     Data to be sent.
+     * @param pduType Type of the PDU.
+     * @throws InvalidPduType if the PDU type is not supported by the bearer.
+     */
+    fun send(pdu: ByteArray, pduType: PduType)
+}
+
+interface Bearer : Transmitter {
+
+    /**
+     * Opens the bearer.
+     */
+    fun open()
+
+    /**
+     * Closes the bearer.
+     */
+    fun close()
+
+    /**
+     * Returns whether the bearer supports the given message type.
+     * @param pduType PDU type.
+     * @return True if the bearer supports the given message type, false otherwise.
+     */
+    fun supports(pduType: PduType): Boolean = runCatching {
+        PduTypes.from(pduType.type)
+    }.isSuccess
+}
+
+/**
+ * Mesh bearer is used to send mesh messages to provisioned nodes.
+ */
+interface MeshBearer : Bearer
