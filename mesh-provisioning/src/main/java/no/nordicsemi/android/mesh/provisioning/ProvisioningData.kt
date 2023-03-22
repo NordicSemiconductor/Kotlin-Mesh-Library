f72391e05913de5a04a940d75ebb2c3cbed7bf58
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2023-03-22 15:01:31 +0100

    Implements a provisioning data class that accumulates provisioning data during the provisioning process.

---
+++ mesh-provisioning/src/main/java/no/nordicsemi/android/mesh/provisioning/ProvisioningData.kt
@@ -0,0 +1,212 @@
+@file:Suppress("MemberVisibilityCanBePrivate", "unused")
+
+package no.nordicsemi.android.mesh.provisioning
+
+import no.nordicsemi.android.mesh.provisioning.ProvisioningError.InvalidPublicKey
+import no.nordicsemi.kotlin.mesh.core.model.*
+import no.nordicsemi.kotlin.mesh.core.util.Utils.toByteArray
+import no.nordicsemi.kotlin.mesh.crypto.Algorithm
+import no.nordicsemi.kotlin.mesh.crypto.Crypto
+import java.security.PrivateKey
+import java.security.PublicKey
+
+/**
+ * This class contains the provisioning data used during the provisioning process.
+ *
+ * @property networkKey                          Network key used during provisioning.
+ * @property ivIndex                             IV index used during provisioning.
+ * @property unicastAddress                      Unicast address used during provisioning.
+ * @property privateKey                          Private key used during provisioning.
+ * @property publicKey                           Public key used during provisioning.
+ * @property sharedSecret                        Shared secret used during provisioning.
+ * @property authValue                           Auth value used during provisioning.
+ * @property deviceConfirmation                  Device confirmation used during provisioning.
+ * @property deviceRandom                        Device random used during provisioning.
+ * @property oobPublicKey                        OOB public key used during provisioning.
+ * @property algorithm                           Algorithm used during provisioning.
+ * @property deviceKey                           Device key used during provisioning.
+ * @property provisionerRandom                   Provisioner random used during provisioning.
+ * @property provisionerPublicKey                Provisioner public key used during provisioning.
+ * @property confirmationInputs                  Confirmation inputs used during provisioning and is
+ *                                               built over the provisioning process. It's composed
+ *                                               of Provisioning Invite PDU, Provisioning
+ *                                               Capabilities PDU,Provisioning Start PDU,
+ *                                               Provisioner's Public Key,Provisionee's Public Key.
+ * @property provisionerConfirmation             Provisioner confirmation used during provisioning.
+ * @property encryptedProvisioningDataWithMic    Encrypted provisioning data with mic used during
+ * @property security                            Security used during provisioning.
+ *                                               provisioning.
+ * @constructor Creates a [ProvisioningData] object.
+ */
+internal class ProvisioningData {
+    private lateinit var networkKey: NetworkKey
+    private lateinit var ivIndex: IvIndex
+    private lateinit var unicastAddress: UnicastAddress
+    private lateinit var privateKey: PrivateKey
+    private lateinit var publicKey: PublicKey
+    private lateinit var sharedSecret: ByteArray
+    private lateinit var authValue: ByteArray
+    private lateinit var deviceConfirmation: ByteArray
+    private lateinit var deviceRandom: ByteArray
+    private var oobPublicKey: Boolean = false
+
+    lateinit var algorithm: Algorithm
+        private set
+    lateinit var deviceKey: ByteArray
+        private set
+    lateinit var provisionerRandom: ByteArray
+        private set
+    lateinit var provisionerPublicKey: ByteArray
+        private set
+
+    private var confirmationInputs = ByteArray(1 + 11 + 5 + 64 + 64)
+
+    val provisionerConfirmation: ByteArray
+        get() = Crypto.calculateConfirmation(
+            confirmationInputs,
+            sharedSecret,
+            provisionerRandom,
+            authValue,
+            algorithm
+        )
+
+    val encryptedProvisioningDataWithMic: ByteArray
+        get() {
+            val keys = Crypto.calculateKeys(
+                algorithm,
+                confirmationInputs,
+                sharedSecret,
+                provisionerRandom,
+                deviceRandom
+            )
+            deviceKey = keys.third
+            val flags = Flags.from(ivIndex, networkKey)
+            val key = when (networkKey.phase) {
+                KeyDistribution -> networkKey.oldKey!!
+                else -> networkKey.key
+            }
+            val data = key + networkKey.index.toByteArray() + ivIndex.index.toByteArray() +
+                    flags.rawValue.toByteArray() + unicastAddress.address.toByteArray()
+            return Crypto.encrypt(data = data, key = keys.first, nonce = keys.second, micSize = 8)
+        }
+
+    val security: Security
+        get() = if (oobPublicKey) Secure else Insecure
+
+    fun prepare(
+        networkKey: NetworkKey,
+        ivIndex: IvIndex,
+        unicastAddress: UnicastAddress,
+    ) {
+        this.networkKey = networkKey
+        this.ivIndex = ivIndex
+        this.unicastAddress = unicastAddress
+    }
+
+    fun generateKeys(algorithm: Algorithm) {
+        Crypto.generateKeyPair(algorithm).also {
+            privateKey = it.private
+            publicKey = it.public
+        }
+
+        provisionerPublicKey = publicKey.encoded
+        this.algorithm = algorithm
+
+        // Generate Provisioner Random
+        provisionerRandom = Crypto.generateRandom(algorithm.length)
+    }
+
+    /**
+     *
+     * This method adds the given PDU to the Provisioning Inputs. Provisioning Inputs are used for
+     * authenticating the Provisioner and the Unprovisioned Device. This method must be called (in
+     * order) for:
+     * - Provisioning Invite,
+     * - Provisioning Capabilities,
+     * - Provisioning Start,
+     * - Provisioner's Public Key,
+     * - Provisionee's Public Key.
+     *
+     * @param pdu provisioning pdu.
+     */
+    fun accumulate(pdu: ByteArray) {
+        confirmationInputs += pdu
+    }
+
+    /**
+     * Invoked when the Provisionee's Public Key has been obtained. This must be called after
+     * generating keys.
+     *
+     * @param publicKey Provisionee's Public Key.
+     * @param usingOob  Indicates whether the Public Key was obtained Out-Of-Band.
+     * @throws InvalidPublicKey if the Provisioner's keys have not been generated.
+     */
+    @Throws(InvalidPublicKey::class)
+    fun onDevicePublicKeyReceived(publicKey: ByteArray, usingOob: Boolean) {
+        runCatching {
+            sharedSecret = Crypto.calculateSharedSecret(privateKey, publicKey)
+            oobPublicKey = usingOob
+        }.onFailure {
+            throw InvalidPublicKey
+        }
+    }
+
+    /**
+     * Invoked when the auth value is received from the device.
+     *
+     * @param authValue auth value
+     */
+    fun onAuthValueReceived(authValue: ByteArray) {
+        this.authValue = authValue
+    }
+
+    /**
+     * Invoked when the device confirmation is received from the device.
+     */
+    fun onDeviceConfirmationReceived(deviceConfirmation: ByteArray) {
+        this.deviceConfirmation = deviceConfirmation
+    }
+
+    /**
+     * Validates the received the Provisioning Confirmation and matches it with one calculated
+     * locally based on the Provisioning received from the device and Auth Value.
+     *
+     * @return true if the confirmation matches, false otherwise.
+     */
+    fun checkIfConfirmationsMatch() =
+        if (deviceRandom.isNotEmpty() && authValue.isNotEmpty() && sharedSecret.isNotEmpty()) {
+            Crypto.calculateConfirmation(
+                confirmationInputs,
+                sharedSecret,
+                deviceRandom,
+                authValue,
+                algorithm
+            ).contentEquals(deviceConfirmation)
+        } else {
+            throw ProvisioningError.InvalidState
+        }
+}
+
+private sealed class Flags(val rawValue: UByte) {
+    private constructor(rawValue: Int) : this(rawValue.toUByte())
+
+    object useNewKeys : Flags(1 shl 0)
+    object ivUpdateActive : Flags(1 shl 1)
+
+    companion object {
+        fun from(ivIndex: IvIndex, networkKey: NetworkKey): Flags {
+            var value: UByte = 0u
+            if (networkKey.phase is UsingNewKeys) {
+                value = value.or((1 shl 0).toUByte())
+            }
+            if (ivIndex.isIvUpdateActive) {
+                value = value.or((1 shl 1).toUByte())
+            }
+            return when (value) {
+                useNewKeys.rawValue -> useNewKeys
+                ivUpdateActive.rawValue -> ivUpdateActive
+                else -> throw IllegalArgumentException("Invalid value")
+            }
+        }
+    }
+}
