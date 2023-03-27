0f940589cb88ad8a928a5c4d75d0a8e117c31e8e
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2023-03-27 10:58:13 +0200

    Fix dependencies

--- mesh-provisioning/build.gradle.kts
+++ mesh-provisioning/build.gradle.kts
@@ -3,6 +3,10 @@ plugins {
 }
 
 dependencies {
-    api(project(":mesh-core"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+
+    implementation(project(":mesh-core"))
+    api(project(":mesh-bearer-provisioning"))
+    implementation(project(":mesh-crypto"))
     testImplementation("junit:junit:4.13.2")
 }
