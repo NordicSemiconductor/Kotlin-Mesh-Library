606ad85607e3389d026f764673da1ecd11fd6abc
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2023-03-27 11:08:17 +0200

    Adds a gatt bearer module to be used by the app module when sending mesh messages over gatt.

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,4 +13,5 @@ plugins {
     alias(libs.plugins.nordic.hilt) apply false
     id("com.android.library") version "7.4.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.8.0" apply false
 }
