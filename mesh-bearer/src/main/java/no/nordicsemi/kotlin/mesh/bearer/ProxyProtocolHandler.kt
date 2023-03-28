37c206fc185ff97ecf3fdb4a45a3809ece9c94e0
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2023-03-28 11:35:11 +0200

    Adds support for a ProxyProtocolHandler that segments and reassembles

--- mesh-bearer/src/main/java/no/nordicsemi/kotlin/mesh/bearer/ProxyProtocolHandler.kt
+++ mesh-bearer/src/main/java/no/nordicsemi/kotlin/mesh/bearer/ProxyProtocolHandler.kt
@@ -1,9 +1,15 @@
-@file:Suppress("unused")
+@file:Suppress("unused", "ArrayInDataClass")
 
 package no.nordicsemi.kotlin.mesh.bearer
 
 import kotlin.experimental.and
 
+/**
+ * Defines the SAR type of the message being sent via the proxy protocol. The messages are segmented
+ * based on the MTU size of the bearer.
+ *
+ * @property rawValue Raw value of the SAR type.
+ */
 private enum class SAR(val rawValue: UByte) {
     COMPLETE_MESSAGE(rawValue = 0b0u),
     FIRST_SEGMENT(rawValue = 0b1u),
@@ -13,10 +19,28 @@ private enum class SAR(val rawValue: UByte) {
     val value: UByte
         get() = (rawValue.toUInt() shl 6).toUByte()
 
+    companion object {
+
+        /**
+         *  Returns the SAR type based on the pdu
+         *
+         *  @param data PDU data
+         *  @return SAR type or null if the data is invalid.
+         */
+        internal fun from(data: ByteArray): SAR? = when (data.first().toUByte()) {
+            COMPLETE_MESSAGE.value -> COMPLETE_MESSAGE
+            FIRST_SEGMENT.value -> FIRST_SEGMENT
+            CONTINUATION.value -> CONTINUATION
+            LAST_SEGMENT.value -> LAST_SEGMENT
+            else -> null
+        }
+    }
 }
 
 /**
  * PDU type identifies the type of the message.
+ *
+ * @return PDU type or null if the data is invalid.
  */
 private fun ByteArray.from(): PduType? {
     if (isEmpty())
@@ -30,10 +54,14 @@ private fun ByteArray.from(): PduType? {
     }
 }
 
+/**
+ * Helper class for handling segmentation and reassembly of messages that are sent via the proxy
+ * protocol. This is defined in Bluetooth Mesh Protocol Specification 1.1
+ */
 class ProxyProtocolHandler {
 
-    private lateinit var buffer: ByteArray
-    private lateinit var bufferTypes: PduType
+    private var buffer: ByteArray? = null
+    private var bufferType: PduType? = null
 
     /**
      * Segments the given data with given message type to 1+ messages where all but the last one are
@@ -45,23 +73,98 @@ class ProxyProtocolHandler {
      * @param mtu   Maximum size of the message.
      * @return Array of segmented messages.
      */
-    fun segment(data: ByteArray, type: PduType, mtu: Int) =
-        if (data.size <= mtu - 1) {
-            var singlePacket = byteArrayOf((SAR.COMPLETE_MESSAGE.value or type.value).toByte())
-            singlePacket += data
-            arrayOf(singlePacket)
-        } else {
-            var packets: Array<ByteArray> = emptyArray()
-            for (i in data.indices step mtu - 1) {
-                val sar = when {
-                    i == 0 -> SAR.FIRST_SEGMENT
-                    i + mtu - 1 >= data.size -> SAR.LAST_SEGMENT
-                    else -> SAR.CONTINUATION
-                }
-                var singlePacket = byteArrayOf((sar.value or type.value).toByte())
-                singlePacket += data.sliceArray(i until minOf(data.size, i + mtu - 1))
-                packets += singlePacket
+    fun segment(data: ByteArray, type: PduType, mtu: Int) = if (data.size <= mtu - 1) {
+        var singlePacket = byteArrayOf((SAR.COMPLETE_MESSAGE.value or type.value).toByte())
+        singlePacket += data
+        arrayOf(singlePacket)
+    } else {
+        var packets: Array<ByteArray> = emptyArray()
+        for (i in data.indices step mtu - 1) {
+            val sar = when {
+                i == 0 -> SAR.FIRST_SEGMENT
+                i + mtu - 1 >= data.size -> SAR.LAST_SEGMENT
+                else -> SAR.CONTINUATION
             }
-            packets
+            var singlePacket = byteArrayOf((sar.value or type.value).toByte())
+            singlePacket += data.sliceArray(i until minOf(data.size, i + mtu - 1))
+            packets += singlePacket
         }
+        packets
+    }
+
+    /**
+     * This method consumes the given data. If the data were segmented, they are buffered until the
+     * last segment is received. This method returns the message and its type when the last segment
+     * (or the only one) has been received, otherwise it returns `null`.
+     *
+     * The packets must be delivered in order. If a new message is received while the previous one is
+     * still reassembled, the old one will be disregarded. Invalid messages are disregarded.
+     *
+     * @param data  The data received.
+     * @return The message and its type, or `null`, if more data are expected.
+     */
+    fun reassemble(data: ByteArray): ReassembledPdu? {
+        require(data.isNotEmpty()) {
+            // Disregard invalid packet.
+            return null
+        }
+
+        val sar = SAR.from(data) ?: run {
+            // Disregard invalid packet.
+            return null
+        }
+        val messageType = PduType.from(data[1].toUByte()) ?: run {
+            // Disregard invalid packet.
+            return null
+        }
+
+        // Ensure, that only complete message or the first segment may be processed if the buffer is
+        // empty.
+        require(buffer != null || sar == SAR.COMPLETE_MESSAGE || sar == SAR.FIRST_SEGMENT) {
+            // Disregard invalid packet.
+            return null
+        }
+
+        // If the packet is a continuation/lastSegment, it should have the same message type as the
+        // current buffer.
+        require(
+            bufferType == null ||
+                    bufferType == messageType ||
+                    sar == SAR.COMPLETE_MESSAGE ||
+                    sar == SAR.FIRST_SEGMENT
+        ) {
+            // Disregard invalid packet.
+            return null
+        }
+
+        // If a new message was received while the old one was processed, disregard the old one.
+        if (buffer != null && (sar == SAR.COMPLETE_MESSAGE || sar == SAR.FIRST_SEGMENT)) {
+            buffer = null
+            bufferType = null
+        }
+
+        // Save the message type and append newly received data.
+        bufferType = messageType
+        if (sar == SAR.COMPLETE_MESSAGE || sar == SAR.LAST_SEGMENT) {
+            buffer = byteArrayOf()
+        }
+        buffer = buffer!! + data.sliceArray(1 until data.size)
+
+        // If the message is complete, return it.
+        // else, just return null.
+        return if (sar == SAR.COMPLETE_MESSAGE || sar == SAR.LAST_SEGMENT) {
+            val tmp = buffer!!
+            buffer = null
+            bufferType = null
+            ReassembledPdu(tmp, messageType)
+        } else null
+    }
 }
+
+/**
+ * Reassembled PDU containing the pdu and the type of the message.
+ *
+ * @property data  Reassembled PDU.
+ * @property type  Type of the message.
+ */
+data class ReassembledPdu(val data: ByteArray, val type: PduType)
