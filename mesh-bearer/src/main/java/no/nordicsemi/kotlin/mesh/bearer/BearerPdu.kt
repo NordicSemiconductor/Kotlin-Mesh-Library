e66a6701fd8cbc8f28be09b917bc6afc233afd25
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2023-04-13 13:54:14 +0200

    Adds missing documentation.

--- mesh-bearer/src/main/java/no/nordicsemi/kotlin/mesh/bearer/BearerPdu.kt
+++ mesh-bearer/src/main/java/no/nordicsemi/kotlin/mesh/bearer/BearerPdu.kt
@@ -6,13 +6,12 @@ package no.nordicsemi.kotlin.mesh.bearer
  * Event emitted when the bearer receives data.
  *
  * @param data   Data that was received.
- * @param type   Type of the data.
+ * @param type   Type of the PDU.
  */
 data class BearerPdu(
     val data: ByteArray,
     val type: PduType
 )
-
 
 /**
  * Bearer event emitted by the bearer.
@@ -21,11 +20,16 @@ sealed class BearerEvent {
 
     /**
      * Event emitted when the bearer is opened.
+     *
+     * @property bearer Bearer implementation sending and receiving mesh PDUs.
      */
     data class OnBearerOpen(val bearer: Bearer) : BearerEvent()
 
     /**
      * Event emitted when the bearer is closed.
+     *
+     * @property bearer     Bearer implementation sending and receiving mesh PDUs.
+     * @property error      Error that caused the bearer to close.
      */
     data class OnBearerClosed(val bearer: Bearer, val error: Throwable) : BearerEvent()
 }
