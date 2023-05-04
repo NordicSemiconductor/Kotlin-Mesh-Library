d2d143adafb045a1ceaec0e433c51e964cda2055
Paul-Andrei Petan <pap36@bath.ac.uk>
2023-05-04 13:00:33 +0200

    Migrate to latest gradle and version catalogue

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,29 +16,11 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("no.nordicsemi.android.gradle:version-catalog:1.3.3")
+            from("no.nordicsemi.android.gradle:version-catalog:1.4.6")
         }
     }
 }
 rootProject.name = "Kotlin-nRF Mesh-Library"
-include (":app")
 include (":core-ui")
-include (":core-data-storage")
-include (":feature-nodes")
-include (":feature-groups")
-include (":feature-settings")
-include (":feature-proxy-filter")
-include (":core-data")
-include (":feature-export")
-include (":feature-network-keys")
-include (":core-common")
-include (":feature-application-keys")
-include (":feature-scenes")
-include (":feature-provisioners")
-
 include (":mesh-core")
 include (":mesh-crypto")
-include (":mesh-provisioning")
-include (":mesh-configuration")
-include (":mesh-generic")
-include (":mesh-lighting")
