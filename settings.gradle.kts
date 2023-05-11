fb93d1903fc857bea8d7997850872e88e685e54a
Paul-Andrei Petan <pap36@bath.ac.uk>
2023-05-11 13:31:40 +0200

    Revert "Migration to new Version Catalogue"
    This reverts commit 52e2d5d6809473f7e520aaff25fb8d7c5bfd21ef.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,10 +16,28 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("no.nordicsemi.android.gradle:version-catalog:1.5.0")
+            from("no.nordicsemi.android.gradle:version-catalog:1.4.6")
         }
     }
 }
+rootProject.name = "Kotlin-nRF Mesh-Library"
+include (":app")
 include (":core-ui")
+include (":core-data-storage")
+include (":feature-nodes")
+include (":feature-groups")
+include (":feature-settings")
+include (":feature-proxy-filter")
+include (":core-data")
+include (":feature-export")
+include (":feature-network-keys")
+include (":core-common")
+include (":feature-application-keys")
+include (":feature-scenes")
+include (":feature-provisioners")
 include (":mesh-core")
 include (":mesh-crypto")
+include (":mesh-provisioning")
+include (":mesh-configuration")
+include (":mesh-generic")
+include (":mesh-lighting")
