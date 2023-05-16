ee151386401bca50cfa79b56f4f106398c5e05ce
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2023-05-16 13:57:02 +0200

    WIP: Provisioning, adds a screen to allow selecting a network key during provisioning

---
+++ app/src/main/java/no/nordicsemi/android/nrfmesh/destinations/NetKeySelectionDestination.kt
@@ -0,0 +1,16 @@
+package no.nordicsemi.android.nrfmesh.destinations
+
+import androidx.hilt.navigation.compose.hiltViewModel
+import no.nordicsemi.android.common.navigation.createDestination
+import no.nordicsemi.android.common.navigation.defineDestination
+import no.nordicsemi.android.nrfmesh.ui.NetKeySelectorRoute
+import no.nordicsemi.android.nrfmesh.viewmodel.NetKeySelectorViewModel
+
+val netKeySelector = createDestination<Int, Int>("netKeySelection")
+
+val netKeySelectorDestination = defineDestination(netKeySelector) {
+    val viewModel: NetKeySelectorViewModel = hiltViewModel()
+    NetKeySelectorRoute(viewModel = viewModel, onBackPressed = { keyIndex ->
+        viewModel.navigateUpWithResult(netKeySelector, keyIndex.toInt())
+    })
+}
