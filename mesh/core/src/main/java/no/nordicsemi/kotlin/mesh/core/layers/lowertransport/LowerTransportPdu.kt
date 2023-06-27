c5e5b8d0c83fe2f4662a6daa80ae3d5b6410167d
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2023-06-27 14:17:31 +0200

    Adds support for LowerTransportPdu

---
+++ mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/layers/lowertransport/LowerTransportPdu.kt
@@ -0,0 +1,29 @@
+@file:Suppress("unused")
+
+package no.nordicsemi.kotlin.mesh.core.layers.lowertransport
+
+import no.nordicsemi.kotlin.mesh.core.layers.network.LowerTransportPduType
+import no.nordicsemi.kotlin.mesh.core.model.MeshAddress
+import no.nordicsemi.kotlin.mesh.core.model.NetworkKey
+
+
+/**
+ * Lower transport layer PDU
+ *
+ * @property source                 Source address of the message.
+ * @property destination            Destination address of the message.
+ * @property networkKey             Network key used to decode/encode the PDU.
+ * @property ivIndex                IV Index used to decode/encode the PDU.
+ * @property type                   Message type.
+ * @property transportPdu           Raw data of the lower transport layer PDU.
+ * @property upperTransportPdu      Raw data of the upper transport layer PDU.
+ */
+internal interface LowerTransportPdu {
+    val source: MeshAddress
+    val destination: MeshAddress
+    val networkKey: NetworkKey
+    val ivIndex: UInt
+    val type: LowerTransportPduType
+    val transportPdu: ByteArray
+    val upperTransportPdu: ByteArray
+}
