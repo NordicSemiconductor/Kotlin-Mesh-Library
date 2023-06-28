e2594d492376486b3574784c7e243b4d1a6a8ce6
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2023-06-28 19:39:11 +0200

    WIP: NetworkLayer, initial commit

--- mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/layers/NetworkManager.kt
+++ mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/layers/NetworkManager.kt
@@ -1,9 +1,21 @@
-@file:Suppress("unused")
+@file:Suppress("unused", "UNUSED_PARAMETER")
 
 package no.nordicsemi.kotlin.mesh.core.layers
 
+import no.nordicsemi.kotlin.mesh.bearer.PduType
+import no.nordicsemi.kotlin.mesh.bearer.Transmitter
 import no.nordicsemi.kotlin.mesh.core.MeshNetworkManager
 import no.nordicsemi.kotlin.mesh.core.layers.network.NetworkLayer
+import no.nordicsemi.kotlin.mesh.core.messages.AcknowledgedMeshMessage
+import no.nordicsemi.kotlin.mesh.core.messages.MeshMessage
+import no.nordicsemi.kotlin.mesh.core.model.ApplicationKey
+import no.nordicsemi.kotlin.mesh.core.model.Element
+import no.nordicsemi.kotlin.mesh.core.model.HasAddress
+import no.nordicsemi.kotlin.mesh.core.model.Model
+import no.nordicsemi.kotlin.mesh.logger.Logger
+import kotlin.concurrent.fixedRateTimer
+import kotlin.time.Duration
+import kotlin.time.DurationUnit
 
 /**
  * Network Manager contains the different layers of the mesh network architecture.
@@ -12,10 +24,87 @@ import no.nordicsemi.kotlin.mesh.core.layers.network.NetworkLayer
  * @constructor Constructs the network manager.
  */
 internal class NetworkManager(private val manager: MeshNetworkManager) {
+    var logger: Logger? = null
+
     var networkLayer = NetworkLayer(this)
     // var lowerTransportLayer = LowerTransportLayer(this)
     // var upperTransportLayer = UpperTransportLayer(this)
     // var accessLayer = AccessLayer(this)
 
+    var transmitter: Transmitter? = manager.transmitter
 
+    var meshNetwork = manager.meshNetwork.replayCache.first()
+
+    var networkParameters = NetworkParameters()
+
+
+    fun handle(incomingPdu: ByteArray, type: PduType) {
+        networkLayer.handle(incomingPdu = incomingPdu, type = type)
+    }
+
+    /**
+     * Publishes the given message using the Publish information from the given Model. If
+     * publication is not set, this message does nothing.
+     *
+     * If publication retransmission is set, this method will retransmit the message specified
+     * number of times, if applicable keeps the same TID value.
+     *
+     * @param message     Message to be published.
+     * @param from        Source model from which the message is originating from.
+     */
+    fun publish(message: MeshMessage, from: Model) {
+        val publish = from.publish ?: return
+        val localElement = from.parentElement ?: return
+        val applicationKey = meshNetwork.applicationKeys[publish.index]
+
+        // calculate the TTL to be used
+        val ttl = when (publish.ttl != 0xFF) {
+            true -> publish.ttl.toUByte()
+            false -> localElement.parentNode?.defaultTTL?.toUByte() ?: networkParameters.defaultTtl
+        }
+
+        send(message, localElement, publish.address, ttl, applicationKey)
+
+        if (message is AcknowledgedMeshMessage) {
+            var count = publish.retransmit.count.toInt()
+            if (count > 0) {
+                val interval: Duration = publish.retransmit.interval
+                fixedRateTimer(
+                    daemon = false,
+                    period = interval.toLong(DurationUnit.MILLISECONDS)
+                ) {
+                    when (--count > 0) {
+                        true -> send(message, localElement, publish.address, ttl, applicationKey)
+                        false -> cancel()
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Encrypts the message with the Application Key and a Network Key bound to it, and sends to the
+     * given destination address.
+     *
+     * This method does not send nor return PDUs to be sent. Instead, for each created segment it
+     * calls transmitter's ``Transmitter/send(_:ofType:)`` method, which should send the PDU over
+     * the air. This is in order to support retransmission in case a packet was lost and needs to be
+     * sent again after block acknowledgment was received.
+     *
+     * @param message         Message to be sent.
+     * @param element         Source Element from which the message is originating from.
+     * @param destination     Destination address.
+     * @param ttl             Initial TTL (Time To Live) value of the message. If `nil`, default
+     *                        Node TTL will be used.
+     * @param key    Application Key to sign the message.
+     */
+    fun send(
+        message: MeshMessage,
+        element: Element,
+        destination: HasAddress,
+        ttl: UByte?,
+        key: ApplicationKey
+    ) {
+        // TODO accessLayer.send(message, srcElement, destination, withTtl, using)
+    }
 }
--- mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/layers/network/NetworkLayer.kt
+++ mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/layers/network/NetworkLayer.kt
@@ -1,6 +1,8 @@
 package no.nordicsemi.kotlin.mesh.core.layers.network
 
+import no.nordicsemi.kotlin.mesh.bearer.PduType
 import no.nordicsemi.kotlin.mesh.core.layers.NetworkManager
+import no.nordicsemi.kotlin.mesh.logger.LogCategory
 
 /**
  * Network Layer of the mesh networking stack
@@ -9,5 +11,34 @@ import no.nordicsemi.kotlin.mesh.core.layers.NetworkManager
  *                          stack.
  * @constructor Constructs the network layer.
  */
-internal class NetworkLayer(val networkManager: NetworkManager) {
+internal class NetworkLayer(private val networkManager: NetworkManager) {
+
+    private val meshNetwork = networkManager.meshNetwork
+    private val logger = networkManager.logger
+
+    fun handle(incomingPdu: ByteArray, type: PduType) {
+        // Discard provisioning pdus as they are handled by the provisioning manager
+        if (type == PduType.PROVISIONING_PDU) return
+
+        if (type != PduType.MESH_BEACON) {
+            // TODO Ensure the PDU has not been handled already
+        }
+
+        when (type) {
+            PduType.NETWORK_PDU -> {
+                NetworkPduDecoder.decode(incomingPdu, type, meshNetwork)?.let { networkPdu ->
+                    logger?.i(LogCategory.NETWORK) { "$networkPdu received" }
+                    // TODO networkManager.lowerTransportLayer.handle(networkPdu)
+                } ?: logger?.w(LogCategory.NETWORK) { "Unable to decode network pdu" }
+            }
+            PduType.MESH_BEACON -> {/*TODO Mesh Beacon decoder*/
+            }
+
+
+            PduType.PROXY_CONFIGURATION -> { /* TODO Proxy configuration decoder*/
+            }
+
+            else -> return
+        }
+    }
 }
