79aca0ad54f958bafd499116b0b9881a7841e3bd
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2023-06-30 15:08:50 +0200

    WIP: NetworkLayer, adds support to a SecureNetworkBeacon decoder.

--- mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/layers/network/SecureNetworkBeacon.kt
+++ mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/layers/network/SecureNetworkBeacon.kt
@@ -3,7 +3,10 @@
 package no.nordicsemi.kotlin.mesh.core.layers.network
 
 import no.nordicsemi.kotlin.mesh.core.model.IvIndex
+import no.nordicsemi.kotlin.mesh.core.model.KeyDistribution
 import no.nordicsemi.kotlin.mesh.core.model.NetworkKey
+import no.nordicsemi.kotlin.mesh.core.util.Utils.toInt
+import no.nordicsemi.kotlin.mesh.crypto.Crypto
 
 /**
  * Defines Secure Network Beacon transmitted by the mesh network.
@@ -42,5 +45,52 @@ internal data class SecureNetworkBeacon(
         result = 31 * result + ivIndex.hashCode()
         result = 31 * result + beaconType.hashCode()
         return result
+    }
+}
+
+internal object SecureNetworkBeaconDecoder {
+
+    fun decode(pdu: ByteArray, networkKey: NetworkKey): SecureNetworkBeacon? {
+
+        require(pdu.size == 22 && pdu[0].toInt() == 1) { return null }
+
+        val keyRefreshFlag = pdu[1].toInt() and 0x01 != 0
+        val updateActive = pdu[1].toInt() and 0x02 != 0
+        val networkId = pdu.sliceArray(2 until 10)
+        val index = pdu.toInt(offset = 10)
+        val ivIndex = IvIndex(index.toUInt(), updateActive)
+        val validForKeyRefreshProcedure = networkKey.oldKey != null
+
+        when {
+            networkId.contentEquals(networkKey.networkId) -> {
+                require(Crypto.authenticate(pdu, networkKey.derivatives.beaconKey)) {
+                    return null
+                }
+                return SecureNetworkBeacon(
+                    pdu = pdu,
+                    networkKey = networkKey,
+                    validForKeyRefreshProcedure = validForKeyRefreshProcedure,
+                    keyRefreshFlag = keyRefreshFlag,
+                    ivIndex = ivIndex
+                )
+            }
+
+            networkKey.phase == KeyDistribution &&
+                    networkId.contentEquals(networkKey.oldNetworkId) &&
+                    networkKey.oldKey != null -> {
+                require(Crypto.authenticate(pdu, networkKey.derivatives.beaconKey)) {
+                    return null
+                }
+                return SecureNetworkBeacon(
+                    pdu = pdu,
+                    networkKey = networkKey,
+                    validForKeyRefreshProcedure = validForKeyRefreshProcedure,
+                    keyRefreshFlag = keyRefreshFlag,
+                    ivIndex = ivIndex
+                )
+            }
+
+            else -> return null
+        }
     }
 }
