8c17a168a04b44f212115a6d8d8caf802045a778
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2023-08-07 15:23:36 +0200

    Adds support for RemoveAddressesFromFilter message.

---
+++ mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/messages/proxy/RemoveAddressesFromFilter.kt
@@ -0,0 +1,44 @@
+@file:Suppress("unused")
+
+package no.nordicsemi.kotlin.mesh.core.messages.proxy
+
+import no.nordicsemi.kotlin.mesh.core.model.ProxyFilterAddress
+import no.nordicsemi.kotlin.mesh.core.model.UnicastAddress
+import no.nordicsemi.kotlin.mesh.core.util.Utils.toByteArray
+import no.nordicsemi.kotlin.mesh.core.util.Utils.toUShort
+
+/**
+ * A message sent by a Proxy Client to remove destination addresses from the proxy filter list.
+ *
+ * @property addresses List of addresses.
+ * "
+ */
+data class RemoveAddressesFromFilter(
+    val addresses: List<ProxyFilterAddress>
+) : AcknowledgedProxyConfigurationMessage {
+    override val opCode: UByte = Decoder.opCode
+    override val responseOpCode: UByte = FilterStatus.opCode
+    override val parameters: ByteArray
+        get() {
+            var byteArray = ByteArray(0)
+            addresses.forEach {
+                byteArray += it.address.toByteArray()
+            }
+            return byteArray
+        }
+
+    companion object Decoder : ProxyConfigurationMessageDecoder {
+        override val opCode: UByte = 0x02u
+        override fun decode(payload: ByteArray) = payload.takeIf {
+            it.size % 2 == 0
+        }?.let {
+            val addresses = mutableListOf<UnicastAddress>()
+            var i = 0
+            while (i < it.size) {
+                addresses.add(UnicastAddress(it.toUShort(i)))
+                i += 2
+            }
+            RemoveAddressesFromFilter(addresses)
+        }
+    }
+}
