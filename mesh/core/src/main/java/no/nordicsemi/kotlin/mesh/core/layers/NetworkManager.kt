b452ec1d790869074159ca1093bbcd0353fa649d
Roshan <rajaratnam.roshan@gmail.com>
2023-08-16 09:13:30 +0200

    Adds support internal API support for sending messages.

--- mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/layers/NetworkManager.kt
+++ mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/layers/NetworkManager.kt
@@ -1,18 +1,23 @@
-@file:Suppress("unused", "UNUSED_PARAMETER")
+@file:Suppress("unused", "UNUSED_PARAMETER", "UNUSED_VARIABLE")
 
 package no.nordicsemi.kotlin.mesh.core.layers
 
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import no.nordicsemi.kotlin.mesh.bearer.PduType
 import no.nordicsemi.kotlin.mesh.bearer.Transmitter
 import no.nordicsemi.kotlin.mesh.core.MeshNetworkManager
 import no.nordicsemi.kotlin.mesh.core.ProxyFilterEventHandler
+import no.nordicsemi.kotlin.mesh.core.layers.access.Busy
 import no.nordicsemi.kotlin.mesh.core.layers.network.NetworkLayer
 import no.nordicsemi.kotlin.mesh.core.messages.AcknowledgedMeshMessage
 import no.nordicsemi.kotlin.mesh.core.messages.MeshMessage
+import no.nordicsemi.kotlin.mesh.core.messages.UnacknowledgedMeshMessage
 import no.nordicsemi.kotlin.mesh.core.messages.proxy.ProxyConfigurationMessage
+import no.nordicsemi.kotlin.mesh.core.model.Address
 import no.nordicsemi.kotlin.mesh.core.model.ApplicationKey
 import no.nordicsemi.kotlin.mesh.core.model.Element
-import no.nordicsemi.kotlin.mesh.core.model.HasAddress
+import no.nordicsemi.kotlin.mesh.core.model.MeshAddress
 import no.nordicsemi.kotlin.mesh.core.model.Model
 import no.nordicsemi.kotlin.mesh.logger.Logger
 import kotlin.concurrent.fixedRateTimer
@@ -25,12 +30,12 @@ import kotlin.time.DurationUnit
  * @property manager Mesh network manager
  * @constructor Constructs the network manager.
  */
-internal class NetworkManager internal constructor(private val manager: MeshNetworkManager) {
+internal class NetworkManager(private val manager: MeshNetworkManager) {
     lateinit var proxy: ProxyFilterEventHandler
 
     var logger: Logger? = null
     val networkPropertiesStorage = manager.networkProperties
-    var networkLayer = NetworkLayer(this)
+    private var networkLayer = NetworkLayer(this)
     // var lowerTransportLayer = LowerTransportLayer(this)
     // var upperTransportLayer = UpperTransportLayer(this)
     // var accessLayer = AccessLayer(this)
@@ -40,6 +45,9 @@ internal class NetworkManager internal constructor(private val manager: MeshNetw
     var meshNetwork = manager.meshNetwork.replayCache.first()
 
     var networkParameters = NetworkParameters()
+    private val mutex = Mutex(locked = true)
+
+    private var outgoingMessages = mutableSetOf<MeshAddress>()
 
     /**
      * Handles the received PDU of a given type.
@@ -72,7 +80,7 @@ internal class NetworkManager internal constructor(private val manager: MeshNetw
             false -> localElement.parentNode?.defaultTTL?.toUByte() ?: networkParameters.defaultTtl
         }
 
-        send(message, localElement, publish.address, ttl, applicationKey)
+        //TODO accessLayer.send(message, localElement, publish.address, ttl, applicationKey, retransmit false)
 
         if (message is AcknowledgedMeshMessage) {
             var count = publish.retransmit.count.toInt()
@@ -82,39 +90,130 @@ internal class NetworkManager internal constructor(private val manager: MeshNetw
                     daemon = false,
                     period = interval.toLong(DurationUnit.MILLISECONDS)
                 ) {
-                    when (--count > 0) {
-                        true -> send(message, localElement, publish.address, ttl, applicationKey)
-                        false -> cancel()
-                    }
+                    if (--count > 0) {
+                        // TODO accessLayer.send(message, localElement, publish.address, ttl, applicationKey, retransmit = true)
+                    } else cancel()
                 }
             }
         }
     }
 
+    @Throws(Busy::class)
+    private suspend fun ensureNotBusy(destination: MeshAddress) = mutex.withLock {
+        require(!outgoingMessages.contains(destination)) { throw Busy }
+        outgoingMessages.add(destination)
+        false
+    }
+
+    suspend fun send(
+        message: MeshMessage,
+        element: Element,
+        destination: MeshAddress,
+        initialTtl: UByte?,
+        key: ApplicationKey
+    ) {
+        try {
+            require(!ensureNotBusy(destination = destination)) { return }
+            // TODO setDeliveryCallback
+            // TODO accessLayer.send(message, element, destination, initialTtl, key, retransmit = false)
+        } catch (e: Exception) {
+            cancel(
+                handler = MessageHandle(
+                    message = message,
+                    source = element.unicastAddress,
+                    destination = destination,
+                    manager = this@NetworkManager
+                )
+            )
+        }
+    }
+
+    suspend fun send(
+        message: AcknowledgedMeshMessage,
+        element: Element,
+        destination: Address,
+        initialTtl: UByte?,
+        key: ApplicationKey
+    ) {
+        val meshAddress = MeshAddress.create(address = destination)
+        try {
+            require(!ensureNotBusy(destination = meshAddress)) { return }
+            // TODO setDeliveryCallback
+            // TODO accessLayer.send(message, element, destination, initialTtl, key, retransmit = true)
+        } catch (e: Exception) {
+            cancel(
+                handler = MessageHandle(
+                    message = message,
+                    source = element.unicastAddress,
+                    destination = meshAddress,
+                    manager = this@NetworkManager
+                )
+            )
+        }
+    }
+
+    suspend fun send(
+        message: UnacknowledgedMeshMessage,
+        element: Element,
+        destination: Address,
+        initialTtl: UByte?
+    ) {
+        val meshAddress = MeshAddress.create(address = destination)
+        try {
+            require(!ensureNotBusy(destination = meshAddress)) { return }
+            // TODO setDeliveryCallback
+            // TODO accessLayer.send(message, element, destination, initialTtl, key, retransmit = true)
+        } catch (e: Exception) {
+            cancel(
+                handler = MessageHandle(
+                    message = message,
+                    source = element.unicastAddress,
+                    destination = meshAddress,
+                    manager = this@NetworkManager
+                )
+            )
+        }
+    }
+
     /**
-     * Encrypts the message with the Application Key and a Network Key bound to it, and sends to the
-     * given destination address.
+     * Encrypts the message with the Device Key and the first Network Key known to it, and sends to
+     * the given destination address.
+     *
+     * The [ConfigNetKeyDelete] will be signed with a different network key than its removing.
      *
      * This method does not send nor return PDUs to be sent. Instead, for each created segment it
      * calls transmitter's ``Transmitter/send(_:ofType:)`` method, which should send the PDU over
      * the air. This is in order to support retransmission in case a packet was lost and needs to be
      * sent again after block acknowledgment was received.
      *
-     * @param message         Message to be sent.
+     * @param configMessage   Config message to be sent.
      * @param element         Source Element from which the message is originating from.
      * @param destination     Destination address.
      * @param ttl             Initial TTL (Time To Live) value of the message. If `nil`, default
      *                        Node TTL will be used.
-     * @param key    Application Key to sign the message.
      */
-    fun send(
-        message: MeshMessage,
+    suspend fun send(
+        configMessage: AcknowledgedMeshMessage,
         element: Element,
-        destination: HasAddress,
-        ttl: UByte?,
-        key: ApplicationKey
+        destination: Address,
+        ttl: UByte?
     ) {
-        // TODO accessLayer.send(message, srcElement, destination, withTtl, using)
+        val meshAddress = MeshAddress.create(address = destination)
+        try {
+            require(!ensureNotBusy(destination = meshAddress)) { return }
+            // TODO setDeliveryCallback
+            // TODO accessLayer.send(message, element, destination, initialTtl, key, retransmit = true)
+
+        } catch (e: Exception) {
+            cancel(
+                handler = MessageHandle(
+                    message = configMessage,
+                    source = element.unicastAddress,
+                    destination = meshAddress,
+                    manager = this@NetworkManager
+                )
+            )
+        }
     }
 
     /**
@@ -124,5 +223,9 @@ internal class NetworkManager internal constructor(private val manager: MeshNetw
      */
     suspend fun send(message: ProxyConfigurationMessage) {
         networkLayer.send(message)
+    }
+
+    fun cancel(handler: MessageHandle) {
+        // TODO accessLayer.cancel(handler)
     }
 }
