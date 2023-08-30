dbfc2c09f1c83cef69f6f240071c899c6fb7bdea
Roshan <rajaratnam.roshan@gmail.com>
2023-08-30 14:41:50 +0200

    Improvements to ControlMessage.

--- mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/layers/lowertransport/ControlMessage.kt
+++ mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/layers/lowertransport/ControlMessage.kt
@@ -4,6 +4,7 @@ package no.nordicsemi.kotlin.mesh.core.layers.lowertransport
 
 import no.nordicsemi.kotlin.mesh.core.layers.network.LowerTransportPduType
 import no.nordicsemi.kotlin.mesh.core.layers.network.NetworkPdu
+import no.nordicsemi.kotlin.mesh.core.layers.uppertransport.HeartbeatMessage
 import no.nordicsemi.kotlin.mesh.core.messages.proxy.ProxyConfigurationMessage
 import no.nordicsemi.kotlin.mesh.core.model.IvIndex
 import no.nordicsemi.kotlin.mesh.core.model.MeshAddress
@@ -33,6 +34,26 @@ internal data class ControlMessage(
 
     override val type = LowerTransportPduType.CONTROL_MESSAGE
 
+    /**
+     * Creates a Control Message.
+     *
+     * @param heartbeatMessage   Heartbeat message.
+     * @param networkKey         Network key.
+     * @constructor Creates a Control Message.
+     */
+    constructor(
+        heartbeatMessage: HeartbeatMessage,
+        networkKey: NetworkKey
+    ) : this(
+        opCode = heartbeatMessage.opCode,
+        upperTransportPdu = heartbeatMessage.transportPdu,
+        source = heartbeatMessage.source,
+        destination = heartbeatMessage.destination as MeshAddress,
+        networkKey = networkKey,
+        ivIndex = heartbeatMessage.ivIndex,
+        ttl = heartbeatMessage.initialTtl
+    )
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
@@ -60,77 +81,77 @@ internal data class ControlMessage(
         result = 31 * result + type.hashCode()
         return result
     }
-}
 
-internal object ControlMessageDecoder {
+    internal companion object {
 
-    /**
-     * Decodes the given [NetworkPdu] to a [ControlMessage].
-     *
-     * @param networkPdu The network pdu to be decoded.
-     * @return ControlMessage or null if the pdu could not be decoded.
-     */
-    fun decode(networkPdu: NetworkPdu) = networkPdu.takeIf {
-        it.transportPdu.isNotEmpty() && it.transportPdu[0] and 0x00.toByte() == 0x00.toByte()
-    }?.let {
-        ControlMessage(
-            opCode = (it.transportPdu[0] and 0x7F.toByte()).toUByte(),
-            upperTransportPdu = it.transportPdu.drop(1).toByteArray(),
-            source = it.source,
-            destination = it.destination,
-            networkKey = it.key,
-            ivIndex = it.ivIndex,
-            ttl = it.ttl
-        )
-    }
-
-    /**
-     * Decodes the given [NetworkPdu] to a [ControlMessage].
-     *
-     * @param segments Array of [SegmentedControlMessage] to be decoded.
-     * @return decoded ControlMessage.
-     */
-    fun decode(segments: Array<SegmentedControlMessage>): ControlMessage {
-        val upperTransportPdu = segments.fold(byteArrayOf()) { acc, segment ->
-            acc + segment.upperTransportPdu
-        }
-        return segments.first().run {
+        /**
+         * Creates a Control Message using the given NetworkPdu.
+         *
+         * @param networkPdu The network pdu to be decoded.
+         * @return ControlMessage or null if the pdu could not be decoded.
+         */
+        fun init(networkPdu: NetworkPdu) = networkPdu.takeIf {
+            it.transportPdu.isNotEmpty() && it.transportPdu[0] and 0x80.toByte() == 0x00.toByte()
+        }?.let {
             ControlMessage(
-                opCode = opCode,
-                upperTransportPdu = upperTransportPdu,
-                source = source,
-                destination = destination,
-                networkKey = networkKey,
-                ivIndex = ivIndex,
-                ttl = ttl
+                opCode = (it.transportPdu[0] and 0x7F.toByte()).toUByte(),
+                upperTransportPdu = it.transportPdu.drop(1).toByteArray(),
+                source = it.source,
+                destination = it.destination,
+                networkKey = it.key,
+                ivIndex = it.ivIndex,
+                ttl = it.ttl
             )
         }
-    }
 
-    /**
-     * Creates a Control Message from the given Proxy configuration message. The source should be
-     * set to the local Node address. The given Network Key should be known to the Proxy node.
-     *
-     * @param message      Proxy configuration message.
-     * @param source       Source address of the message.
-     * @param networkKey   Network key to be used to encrypt the message.
-     * @param ivIndex      Current IV Index of the network.
-     *
-     * @return decoded ControlMessage.
-     */
-    fun decode(
-        message: ProxyConfigurationMessage,
-        source: MeshAddress,
-        networkKey: NetworkKey,
-        ivIndex: IvIndex
-    ) = ControlMessage(
-        opCode = message.opCode,
-        upperTransportPdu = message.parameters ?: byteArrayOf(),
-        source = source,
-        destination = UnassignedAddress,
-        networkKey = networkKey,
-        ivIndex = ivIndex.transmitIvIndex,
-        ttl = 0u
-    )
+        /**
+         * Creates a Control Message from a given array of message segments.
+         *
+         * @param segments Array of [SegmentedControlMessage] to be decoded.
+         * @return a ControlMessage.
+         */
+        fun init(segments: Array<SegmentedControlMessage>): ControlMessage {
+            val upperTransportPdu = segments.fold(byteArrayOf()) { acc, segment ->
+                acc + segment.upperTransportPdu
+            }
+            return segments.first().run {
+                ControlMessage(
+                    opCode = opCode,
+                    upperTransportPdu = upperTransportPdu,
+                    source = source,
+                    destination = destination,
+                    networkKey = networkKey,
+                    ivIndex = ivIndex,
+                    ttl = ttl
+                )
+            }
+        }
+
+        /**
+         * Creates a Control Message from the given Proxy configuration message. The source should be
+         * set to the local Node address. The given Network Key should be known to the Proxy node.
+         *
+         * @param message      Proxy configuration message.
+         * @param source       Source address of the message.
+         * @param networkKey   Network key to be used to encrypt the message.
+         * @param ivIndex      Current IV Index of the network.
+         *
+         * @return decoded ControlMessage.
+         */
+        fun init(
+            message: ProxyConfigurationMessage,
+            source: MeshAddress,
+            networkKey: NetworkKey,
+            ivIndex: IvIndex
+        ) = ControlMessage(
+            opCode = message.opCode,
+            upperTransportPdu = message.parameters ?: byteArrayOf(),
+            source = source,
+            destination = UnassignedAddress,
+            networkKey = networkKey,
+            ivIndex = ivIndex.transmitIvIndex,
+            ttl = 0u
+        )
+    }
 }
 
