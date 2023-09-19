cabe0cb317791f207f5f83423bfb5e79ff2f1715
Roshan <rajaratnam.roshan@gmail.com>
2023-09-19 13:18:55 +0200

    Adds support for ConfigNodeReset message.

---
+++ mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/messages/foundation/configuration/ConfigNodeResetStatus.kt
@@ -0,0 +1,25 @@
+@file:Suppress("ConvertSecondaryConstructorToPrimary", "unused")
+
+package no.nordicsemi.kotlin.mesh.core.messages.foundation.configuration
+
+import no.nordicsemi.kotlin.mesh.core.messages.BaseMeshMessage
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigMessageInitializer
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigResponse
+
+/**
+ * Status message received when after sending a [ConfigNodeReset] message.
+ */
+class ConfigNodeResetStatus : ConfigResponse {
+    override val opCode: UInt = Initializer.opCode
+    override val parameters: ByteArray? = null
+
+    companion object Initializer : ConfigMessageInitializer {
+        override val opCode: UInt = 0x804Au
+
+        override fun init(parameters: ByteArray): BaseMeshMessage? = parameters.takeIf {
+            it.isNotEmpty()
+        }?.let {
+            ConfigNodeResetStatus()
+        }
+    }
+}
