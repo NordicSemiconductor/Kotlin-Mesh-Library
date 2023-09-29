871292e700f57bdbc061f0179a0b01b7c3021afa
Roshan <rajaratnam.roshan@gmail.com>
2023-09-29 11:43:49 +0200

    Add helper method

--- mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/layers/NetworkParameters.kt
+++ mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/layers/NetworkParameters.kt
@@ -1,7 +1,9 @@
-@file:Suppress("unused")
+@file:Suppress("unused", "MemberVisibilityCanBePrivate")
 
 package no.nordicsemi.kotlin.mesh.core.layers
 
+import kotlin.math.max
+import kotlin.math.min
 import kotlin.time.Duration
 import kotlin.time.DurationUnit
 import kotlin.time.toDuration
@@ -12,10 +14,10 @@ import kotlin.time.toDuration
  * Network parameters configure the transmission and retransmission intervals, acknowledge message
  * timeout, the default Time To Live (TTL) and others.
  *
- * @property defaultTtl                         Default Time To Live (TTL) will be used for
- *                                              sending messages. If the value has not been set in
- *                                              the Provisioner's Node. By default it is set to 5,
- *                                              which is a reasonable value. The TTL shall be in
+ * @property defaultTtl                         Default Time To Live (TTL) will be used for sending
+ *                                              messages. If the value has not been set in the
+ *                                              Provisioner's Node. By default it is set to 5, which
+ *                                              is a reasonable value. The TTL shall be in
  *                                              range 2...127.
  *
  * @property incompleteMessageTimeout           Timeout after which an incomplete segmented message
@@ -76,22 +78,224 @@ import kotlin.time.toDuration
  *                                              count. The TTL and segment count dependent parts are
  *                                              added automatically, and this value shall specify
  *                                              only the constant part.
- *
- *
- *
- * The acknowledgement message time interval.
  * @constructor Constructs a NetworkParameters object.
- *
  */
+// TODO Clarify API and documentation.
 data class NetworkParameters(
-    var defaultTtl: UByte = 5u,
-    var incompleteMessageTimeout: Duration = 10.toDuration(DurationUnit.SECONDS),
-    var acknowledgementTimeInterval: Duration = 0.150.toDuration(DurationUnit.SECONDS),
-    var transmissionTimeInterval: Duration = 0.200.toDuration(DurationUnit.SECONDS),
-    var retransmissionLimit: Int = 5,
-    var acknowledgementMessageTimeout: Duration = 30.toDuration(DurationUnit.SECONDS),
-    var acknowledgementMessageTimeInterval: Duration = 2.toDuration(DurationUnit.SECONDS)
+    private var _defaultTtl: UByte = 5u,
+    private var _sarDiscardTimeout: UByte = 0b001u,                                  // (n+1)*5 sec = 10 seconds.
+    private var _sarAcknowledgementDelayIncrement: UByte = 0b001u,                   // (n+1.5) = 2.5
+    private var _sarReceiverSegmentIntervalStep: UByte = 0b0101u,                    // (n+1)*10 ms = 20 ms
+    private var _sarSegmentsThreshold: UByte = 0b00011u,                             // 3
+    private var _sarAcknowledgementRetransmissionCount: UByte = 0b00u,               // 0
+    private var _sarSegmentIntervalStep: UByte = 0b0101u,                            // (n+1)*10 ms = 60 ms
+    private var _sarUnicastRetransmissionsCount: UByte = 0b0010u,                    // 3
+    private var _sarUnicastRetransmissionsWithoutProgressCount: UByte = 0b0010u,     // 3
+    private var _sarUnicastRetransmissionsIntervalStep: UByte = 0b0111u,             // (n+1)*25 ms = 200 ms
+    private var _sarUnicastRetransmissionsIntervalIncrement: UByte = 0b0001u,         // (n+1)*25 ms = 50 ms
+    private var _sarMulticastRetransmissionsCount: UByte = 0b0010u,                  // 3
+    private var _sarMulticastRetransmissionsIntervalStep: UByte = 0b1001u,           // (n+1)*25 ms = 250 ms
+    private var _acknowledgementMessageTimeout: Duration = 30.toDuration(DurationUnit.SECONDS),
+    private var _acknowledgementMessageInterval: Duration = 2.toDuration(DurationUnit.SECONDS)
 ) {
+
+    var defaultTtl: UByte
+        get() = _defaultTtl
+        private set(value) {
+            _defaultTtl = max(2, min(value.toInt(), 127)).toUByte()
+        }
+
+    var discardTimeout: Duration
+        get() = ((_sarDiscardTimeout + 1u).toInt() * 5).toDuration(DurationUnit.SECONDS)
+        set(value) {
+            _sarDiscardTimeout =
+                (min(5, value.toInt(DurationUnit.SECONDS) / 5).toUByte() - 1u).toUByte()
+        }
+
+    var sarDiscardTimeout: UByte
+        get() = _sarDiscardTimeout
+        set(value) {
+            _sarDiscardTimeout =
+                min(value.toInt(), 0b1111).toUByte() // valid range 0..15
+        }
+
+    fun setAcknowledgementTimerInterval(
+        segmentReceptionInterval: Duration,
+        acknowledgementDelayIncrement: Double
+    ) {
+        val min1 = min(
+            a = 0.16,
+            b = (segmentReceptionInterval.toDouble(DurationUnit.MILLISECONDS)) * 100
+        )
+        _sarReceiverSegmentIntervalStep = ((max(a = 0.01, b = min1) * 100) - 1.0).toInt().toUByte()
+
+        val min2 = min(8.5, acknowledgementDelayIncrement)
+
+        _sarAcknowledgementDelayIncrement = max(0.0, max(1.5, min2) - 1.5).toInt().toUByte()
+    }
+
+    var sarAcknowledgementDelayIncrement: UByte
+        get() = _sarAcknowledgementDelayIncrement
+        set(value) {
+            _sarAcknowledgementDelayIncrement = min(value.toInt(), 0b111).toUByte()
+        }
+
+    var sarReceiverSegmentIntervalStep: UByte
+        get() = _sarReceiverSegmentIntervalStep
+        set(value) {
+            _sarReceiverSegmentIntervalStep = min(value.toInt(), 0b1111).toUByte()
+        }
+
+    var acknowledgementDelayIncrement: Double
+        get() = _sarAcknowledgementDelayIncrement.toDouble() + 1.5
+        set(value) {
+            val min1 = min(8.5, acknowledgementDelayIncrement)
+            _sarAcknowledgementDelayIncrement = max(0.0, max(1.5, min1) - 1.5).toInt().toUByte()
+        }
+
+    var segmentReceptionInterval: Duration
+        get() = ((_sarReceiverSegmentIntervalStep + 1u).toInt() * 10).toDuration(DurationUnit.MILLISECONDS)
+        set(value) {
+            val min1 = min(
+                a = 0.16,
+                b = (value.toDouble(DurationUnit.MILLISECONDS)) * 100
+            )
+            _sarReceiverSegmentIntervalStep =
+                ((max(a = 0.01, b = min1) * 100) - 1.0).toInt().toUByte()
+        }
+
+    internal fun acknowledgementTimerInterval(segN: UByte): Duration {
+        val min = min(segN.toDouble() + 0.5, acknowledgementDelayIncrement)
+        val duration = min * segmentReceptionInterval.toDouble(DurationUnit.SECONDS)
+        return duration.toDuration(DurationUnit.SECONDS)
+    }
+
+    internal val completeAcknowledgementTimerInterval: Duration
+        get() = (acknowledgementDelayIncrement * (segmentReceptionInterval
+            .toDouble(unit = DurationUnit.SECONDS)))
+            .toDuration(DurationUnit.SECONDS)
+
+
+    fun retransmitSegmentAcknowledgementMessages(count: UByte, threshold: UByte) {
+        _sarSegmentsThreshold = threshold
+        _sarAcknowledgementRetransmissionCount = count
+    }
+
+    var sarSegmentsThreshold: UByte
+        get() = _sarSegmentsThreshold
+        set(value) {
+            _sarSegmentsThreshold = min(value.toInt(), 0b11111).toUByte()
+        }
+
+    var sarAcknowledgementRetransmissionCount: UByte
+        get() = _sarAcknowledgementRetransmissionCount
+        set(value) {
+            _sarAcknowledgementRetransmissionCount = min(value.toInt(), 0b11).toUByte()
+        }
+
+    var sarSegmentIntervalStep: UByte
+        get() = _sarSegmentIntervalStep
+        set(value) {
+            _sarSegmentIntervalStep = min(value.toInt(), 0b1111).toUByte()
+        }
+
+    var segmentTransmissionInterval: Duration
+        get() = ((_sarSegmentIntervalStep + 1u).toInt() * 0.01).toDuration(DurationUnit.SECONDS)
+        set(value) {
+            val max = max(value.toDouble(DurationUnit.MILLISECONDS), 0.01)
+            val min = min(0.16, max)
+            _sarSegmentIntervalStep = ((min * 100).toInt() - 1).toUByte()
+        }
+
+    var sarUnicastRetransmissionsCount: UByte
+        get() = _sarUnicastRetransmissionsCount
+        set(value) {
+            _sarUnicastRetransmissionsCount = min(value.toInt(), 0b1111).toUByte()
+        }
+
+    var sarUnicastRetransmissionsWithoutProgressCount: UByte
+        get() = _sarUnicastRetransmissionsWithoutProgressCount
+        set(value) {
+            _sarUnicastRetransmissionsWithoutProgressCount = min(value.toInt(), 0b1111).toUByte()
+        }
+
+    var sarUnicastRetransmissionsIntervalStep: UByte
+        get() = _sarUnicastRetransmissionsIntervalStep
+        set(value) {
+            _sarUnicastRetransmissionsIntervalStep = min(value.toInt(), 0b1111).toUByte()
+        }
+
+    var unicastRetransmissionsIntervalStep: Duration
+        get() = ((_sarUnicastRetransmissionsIntervalStep + 1u).toInt() * 0.025).toDuration(
+            DurationUnit.SECONDS
+        )
+        set(value) {
+            val max = max(value.toDouble(DurationUnit.MILLISECONDS), 0.025)
+            val min = min(max, 0.4)
+            _sarUnicastRetransmissionsIntervalStep = ((min * 40).toInt() - 1).toUByte()
+        }
+
+    var sarUnicastRetransmissionsIntervalIncrement: UByte
+        get() = _sarUnicastRetransmissionsIntervalIncrement
+        set(value) {
+            _sarUnicastRetransmissionsIntervalIncrement = min(value.toInt(), 0b1111).toUByte()
+        }
+
+    var unicastRetransmissionsIntervalIncrement: Duration
+        get() = ((_sarUnicastRetransmissionsIntervalIncrement + 1u).toInt() * 0.025).toDuration(
+            DurationUnit.SECONDS
+        )
+        set(value) {
+            val max = max(value.toDouble(DurationUnit.MILLISECONDS), 0.025)
+            val min = min(0.4, max)
+            _sarUnicastRetransmissionsIntervalIncrement = ((min * 40).toInt() - 1).toUByte()
+        }
+
+    internal fun unicastRetransmissionsInterval(ttl: UByte) = if (ttl == 0.toUByte())
+        unicastRetransmissionsIntervalStep
+    else unicastRetransmissionsIntervalStep +
+            (unicastRetransmissionsIntervalIncrement * (ttl.toDouble() - 1))
+
+    var sarMulticastRetransmissionsCount: UByte
+        get() = _sarMulticastRetransmissionsCount
+        set(value) {
+            _sarMulticastRetransmissionsCount = min(value.toInt(), 0b1111).toUByte()
+        }
+
+    var sarMulticastRetransmissionsIntervalStep: UByte
+        get() = _sarMulticastRetransmissionsIntervalStep
+        set(value) {
+            _sarMulticastRetransmissionsIntervalStep = min(value.toInt(), 0b1111).toUByte()
+        }
+
+    var multicastRetransmissionsInterval : Duration
+        get() = ((_sarMulticastRetransmissionsIntervalStep + 1u).toInt() * 0.025).toDuration(
+            DurationUnit.SECONDS
+        )
+        set(value) {
+            val max = max(value.toDouble(DurationUnit.MILLISECONDS), 0.025)
+            val min = min(max, 0.4)
+            _sarMulticastRetransmissionsIntervalStep = ((min * 40).toInt() - 1).toUByte()
+        }
+
+    var acknowledgementMessageTimeout: Duration
+        get() = _acknowledgementMessageTimeout
+        set(value) {
+            _acknowledgementMessageTimeout = max(30.0, value.toDouble(DurationUnit.SECONDS))
+                .toDuration(DurationUnit.SECONDS)
+        }
+
+    var acknowledgementMessageTimeInterval: Duration
+        get() = _acknowledgementMessageInterval
+        set(value) {
+            _acknowledgementMessageInterval = max(2.0, value.toDouble(DurationUnit.SECONDS))
+                .toDuration(DurationUnit.SECONDS)
+        }
+
+    internal fun acknowledgementMessageInterval(ttl: UByte, segmentCount: Int) =
+        _acknowledgementMessageInterval +
+                ((ttl.toDouble() * 0.050) + (segmentCount.toDouble() * 0.050))
+                    .toDuration(DurationUnit.SECONDS)
 
     /**
      * According to Bluetooth Mesh Profile 1.0.1, section 3.10.5, if the IV Index of the mesh
@@ -115,15 +319,6 @@ data class NetworkParameters(
      */
     var ivUpdateTestMode = false
 }
-
-
-
-
-
-
-
-
-
 
 
 
