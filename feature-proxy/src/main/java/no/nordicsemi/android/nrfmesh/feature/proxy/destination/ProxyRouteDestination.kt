a76c64068a4cf96ff48a45e2605957a9a1a31699
Roshan <rajaratnam.roshan@gmail.com>
2023-10-11 23:44:50 +0200

    refactoring

--- feature-proxy/src/main/java/no/nordicsemi/android/nrfmesh/feature/proxy/destination/ProxyRouteDestination.kt
+++ feature-proxy/src/main/java/no/nordicsemi/android/nrfmesh/feature/proxy/destination/ProxyRouteDestination.kt
@@ -4,14 +4,14 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import no.nordicsemi.android.common.navigation.createSimpleDestination
 import no.nordicsemi.android.common.navigation.defineDestination
 import no.nordicsemi.android.nrfmesh.feature.proxy.ProxyFilterRoute
-import no.nordicsemi.android.nrfmesh.feature.proxy.ProxyRouteViewModel
+import no.nordicsemi.android.nrfmesh.feature.proxy.viewmodel.ProxyRouteViewModel
 
 val proxyFilter = createSimpleDestination("proxy_filter")
 
 val proxyFilterDestination = defineDestination(proxyFilter) {
     val viewModel: ProxyRouteViewModel = hiltViewModel()
 
-    ProxyFilterRoute(viewModel = viewModel)
+    ProxyFilterRoute()
 }
 
 val proxyFilterDestinations = listOf(proxyFilterDestination)
