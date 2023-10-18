c0fa1e622ffa6f4c27f4311a5f0c4fd9cede0cc4
Roshan <rajaratnam.roshan@gmail.com>
2023-10-18 13:29:22 +0200

    Adds support for automatic connectivity.

--- core-data/src/main/java/no/nordicsemi/android/nrfmesh/core/data/CoreDataRepository.kt
+++ core-data/src/main/java/no/nordicsemi/android/nrfmesh/core/data/CoreDataRepository.kt
@@ -1,32 +1,51 @@
 package no.nordicsemi.android.nrfmesh.core.data
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.os.ParcelUuid
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import no.nordicsemi.android.kotlin.ble.core.ServerDevice
+import no.nordicsemi.android.kotlin.ble.core.scanner.BleScanResult
+import no.nordicsemi.android.kotlin.ble.scanner.BleScanner
+import no.nordicsemi.android.kotlin.mesh.bearer.android.utils.MeshProxyService
 import no.nordicsemi.android.kotlin.mesh.bearer.pbgatt.PbGattBearer
 import no.nordicsemi.android.nrfmesh.core.common.dispatchers.Dispatcher
 import no.nordicsemi.android.nrfmesh.core.common.dispatchers.MeshDispatchers
 import no.nordicsemi.kotlin.mesh.bearer.Bearer
+import no.nordicsemi.kotlin.mesh.bearer.BearerEvent
 import no.nordicsemi.kotlin.mesh.bearer.gatt.GattBearer
 import no.nordicsemi.kotlin.mesh.core.MeshNetworkManager
+import no.nordicsemi.kotlin.mesh.core.model.MeshNetwork
 import no.nordicsemi.kotlin.mesh.core.model.Provisioner
 import no.nordicsemi.kotlin.mesh.core.model.UnicastAddress
 import no.nordicsemi.kotlin.mesh.core.model.UnicastRange
 import no.nordicsemi.kotlin.mesh.core.model.serialization.config.NetworkConfiguration
+import no.nordicsemi.kotlin.mesh.core.util.networkIdentity
+import no.nordicsemi.kotlin.mesh.core.util.nodeIdentity
 import javax.inject.Inject
 
 class CoreDataRepository @Inject constructor(
+    @ApplicationContext private val context: Context,
     private val meshNetworkManager: MeshNetworkManager,
+    private val scanner: BleScanner,
     @Dispatcher(MeshDispatchers.IO) private val ioDispatcher: CoroutineDispatcher
 ) {
 
+    private val proxyScanner = CoroutineScope(Dispatchers.Main + SupervisorJob())
     val network = meshNetworkManager.meshNetwork
 
-    /*val network: Flow<MeshNetwork> = flow {
-        meshNetworkManager.meshNetwork?.let { emit(it) }
-    }*/
     private var bearer: Bearer? = null
+    var autoConnectProxy = true
+        private set
 
     suspend fun load() = withContext(ioDispatcher) {
         if (!meshNetworkManager.load()) {
@@ -50,13 +69,8 @@ class CoreDataRepository @Inject constructor(
 
     suspend fun connectOverPbGattBearer(context: Context, device: ServerDevice) =
         withContext(ioDispatcher) {
-            if (bearer is GattBearer) {
-                bearer?.close()
-            }
-            PbGattBearer(
-                context = context,
-                device = device
-            ).also {
+            if (bearer is GattBearer) bearer?.close()
+            PbGattBearer(context = context, device = device).also {
                 it.open()
                 bearer = it
             }
@@ -64,13 +78,8 @@ class CoreDataRepository @Inject constructor(
 
     suspend fun connectOverGattBearer(context: Context, device: ServerDevice) =
         withContext(ioDispatcher) {
-            if (bearer is PbGattBearer) {
-                bearer?.close()
-            }
-            GattBearer(
-                context = context,
-                device = device
-            ).also {
+            if (bearer is PbGattBearer) bearer?.close()
+            GattBearer(context = context, device = device).also {
                 it.open()
                 bearer = it
             }
@@ -79,4 +88,70 @@ class CoreDataRepository @Inject constructor(
     suspend fun disconnect() = withContext(ioDispatcher) {
         bearer?.close()
     }
+
+    /**
+     * Starts automatic connectivity to the proxy node.
+     *
+     * @param meshNetwork Mesh network required to match the proxy node.
+     */
+    fun startAutomaticConnectivity(meshNetwork: MeshNetwork?) {
+        proxyScanner.launch {
+            connect(meshNetwork)
+        }
+    }
+
+    /**
+     * Scans and connects to the proxy node if found.
+     *
+     * @param meshNetwork Mesh network required to match the proxy node.
+     */
+    private tailrec suspend fun connect(meshNetwork: MeshNetwork?) {
+        if (autoConnectProxy) {
+            val device = scanForProxy(meshNetwork)
+            val bearer = connectOverGattBearer(context = context, device = device)
+            bearer.state.filter { it is BearerEvent.Closed }.first()
+            connect(meshNetwork)
+        }
+    }
+
+    /**
+     * Scans for the proxy node.
+     *
+     * @param meshNetwork Mesh network required to match the proxy node.
+     * @return [BleScanResult] containing the proxy node.
+     */
+    @SuppressLint("MissingPermission")
+    suspend fun scanForProxy(meshNetwork: MeshNetwork?): ServerDevice = scanner.scan().first {
+        val data = it.data?.scanRecord?.serviceData
+            ?.get(ParcelUuid(MeshProxyService.uuid))
+        meshNetwork?.takeIf {
+            data != null
+        }?.let { meshNetwork ->
+            data!!.value.nodeIdentity()?.let { nodeIdentity ->
+                meshNetwork.matches(nodeIdentity)
+            } ?: false || data.value.networkIdentity()?.let { networkId ->
+                meshNetwork.matches(networkId)
+            } ?: false
+        } ?: false
+    }.device
+
+    /**
+     * Stops the proxy scanner.
+     */
+    fun stopProxyScanner() {
+        proxyScanner.cancel("Scanner stopped")
+    }
+
+    /**
+     * Enables or disables automatic connectivity to the proxy node.
+     *
+     * @param meshNetwork Mesh network required to match the proxy node.
+     * @param enabled     True to enable, false to disable.
+     */
+    fun enableAutoConnectProxy(meshNetwork: MeshNetwork?, enabled: Boolean) {
+        autoConnectProxy = enabled
+        if (enabled) startAutomaticConnectivity(meshNetwork)
+        else stopProxyScanner()
+    }
+
 }
--- core-data/src/main/java/no/nordicsemi/android/nrfmesh/core/data/di/DataStoreRepositoryModule.kt
+++ core-data/src/main/java/no/nordicsemi/android/nrfmesh/core/data/di/DataStoreRepositoryModule.kt
@@ -1,10 +1,13 @@
 package no.nordicsemi.android.nrfmesh.core.data.di
 
+import android.content.Context
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineDispatcher
+import no.nordicsemi.android.kotlin.ble.scanner.BleScanner
 import no.nordicsemi.android.nrfmesh.core.common.dispatchers.Dispatcher
 import no.nordicsemi.android.nrfmesh.core.common.dispatchers.MeshDispatchers
 import no.nordicsemi.android.nrfmesh.core.data.CoreDataRepository
@@ -18,9 +21,12 @@ object DataStoreRepositoryModule {
     @Singleton
     @Provides
     fun provideDataStoreRepository(
+        @ApplicationContext context: Context,
         meshNetworkManager: MeshNetworkManager,
         @Dispatcher(MeshDispatchers.IO) ioDispatcher: CoroutineDispatcher
     ) = CoreDataRepository(
+        context = context,
+        scanner = BleScanner(context),
         meshNetworkManager = meshNetworkManager,
         ioDispatcher = ioDispatcher
     )
