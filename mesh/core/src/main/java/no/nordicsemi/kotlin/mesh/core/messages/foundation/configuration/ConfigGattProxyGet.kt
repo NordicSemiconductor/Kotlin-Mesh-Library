41ac787c8e9dce753ca5aaa0efae5bf8f0398037
Roshan <rajaratnam.roshan@gmail.com>
2023-12-06 09:58:59 +0100

    ConfigGattProxyGet should be a class

--- mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/messages/foundation/configuration/ConfigGattProxyGet.kt
+++ mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/messages/foundation/configuration/ConfigGattProxyGet.kt
@@ -7,16 +7,16 @@ import no.nordicsemi.kotlin.mesh.core.messages.ConfigMessageInitializer
  * Defines message sent to request the Current GATT Proxy state of the Node. [ConfigGattProxyStatus]
  * will be the response to this message.
  */
-data object ConfigGattProxyGet : AcknowledgedConfigMessage {
+class ConfigGattProxyGet : AcknowledgedConfigMessage {
     override val opCode = Initializer.opCode
     override val parameters = null
     override val responseOpCode: UInt = ConfigGattProxyStatus.opCode
 
-    internal object Initializer : ConfigMessageInitializer {
+    companion object Initializer : ConfigMessageInitializer {
         override val opCode = 0x8012u
 
         override fun init(parameters: ByteArray?) = parameters?.let {
-            ConfigGattProxyGet
+            ConfigGattProxyGet()
         }
     }
 }
