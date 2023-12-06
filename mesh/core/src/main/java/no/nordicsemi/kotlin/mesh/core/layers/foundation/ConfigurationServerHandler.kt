87c5c08e29d81b861881ac8df80f366314668083
Roshan <rajaratnam.roshan@gmail.com>
2023-12-06 13:17:03 +0100

    Adds supported message types. Note: this is WIP

--- mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/layers/foundation/ConfigurationServerHandler.kt
+++ mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/layers/foundation/ConfigurationServerHandler.kt
@@ -2,16 +2,28 @@
 
 package no.nordicsemi.kotlin.mesh.core.layers.foundation
 
-import no.nordicsemi.kotlin.mesh.core.messages.HasInitializer
+import no.nordicsemi.kotlin.mesh.core.messages.foundation.configuration.ConfigCompositionDataGet
+import no.nordicsemi.kotlin.mesh.core.messages.foundation.configuration.ConfigGattProxyGet
+import no.nordicsemi.kotlin.mesh.core.messages.foundation.configuration.ConfigHeartbeatPublicationGet
+import no.nordicsemi.kotlin.mesh.core.messages.foundation.configuration.ConfigHeartbeatPublicationSet
+import no.nordicsemi.kotlin.mesh.core.messages.foundation.configuration.ConfigModelPublicationGet
+import no.nordicsemi.kotlin.mesh.core.messages.foundation.configuration.ConfigModelPublicationSet
+import no.nordicsemi.kotlin.mesh.core.messages.foundation.configuration.ConfigNetKeyDelete
+import no.nordicsemi.kotlin.mesh.core.messages.foundation.configuration.ConfigNodeReset
 import no.nordicsemi.kotlin.mesh.core.model.MeshNetwork
-import no.nordicsemi.kotlin.mesh.core.util.MessageComposer
 import no.nordicsemi.kotlin.mesh.core.util.ModelEventHandler
 
 class ConfigurationServerHandler(override val meshNetwork: MeshNetwork) : ModelEventHandler() {
-    override val messageTypes: Map<UInt, HasInitializer>
-        get() = TODO("Not yet implemented")
-    override val isSubscriptionSupported: Boolean
-        get() = TODO("Not yet implemented")
-    override val publicationMessageComposer: MessageComposer
-        get() = TODO("Not yet implemented")
+    override val messageTypes = mapOf(
+        ConfigCompositionDataGet.opCode to ConfigCompositionDataGet,
+        ConfigNetKeyDelete.opCode to ConfigNetKeyDelete,
+        ConfigNodeReset.opCode to ConfigNodeReset,
+        ConfigModelPublicationGet.opCode to ConfigModelPublicationGet,
+        ConfigModelPublicationSet.opCode to ConfigModelPublicationSet,
+        ConfigHeartbeatPublicationGet.opCode to ConfigHeartbeatPublicationGet,
+        ConfigHeartbeatPublicationSet.opCode to ConfigHeartbeatPublicationSet,
+        ConfigGattProxyGet.opCode to ConfigGattProxyGet
+    )
+    override val isSubscriptionSupported = false
+    override val publicationMessageComposer = null
 }
