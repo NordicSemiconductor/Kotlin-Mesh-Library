9bf8650a4f6e455179977e0b0e88dc7f196eef69
Roshan <rajaratnam.roshan@gmail.com>
2024-01-11 10:29:56 +0100

    clean up

--- feature-provisioners/src/main/java/no/nordicsemi/android/nrfmesh/feature/provisioners/ranges/RangesRoute.kt
+++ feature-provisioners/src/main/java/no/nordicsemi/android/nrfmesh/feature/provisioners/ranges/RangesRoute.kt
@@ -3,7 +3,6 @@
 package no.nordicsemi.android.nrfmesh.feature.provisioners.ranges
 
 import android.annotation.SuppressLint
-import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
