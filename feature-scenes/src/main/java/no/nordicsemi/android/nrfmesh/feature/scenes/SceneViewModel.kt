1e21af5d74bece65344376bf7c69de94234b6061
Roshan <rajaratnam.roshan@gmail.com>
2024-01-11 10:21:54 +0100

    Fix imports

--- feature-scenes/src/main/java/no/nordicsemi/android/nrfmesh/feature/scenes/SceneViewModel.kt
+++ feature-scenes/src/main/java/no/nordicsemi/android/nrfmesh/feature/scenes/SceneViewModel.kt
@@ -5,13 +5,10 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import no.nordicsemi.android.common.navigation.Navigator
