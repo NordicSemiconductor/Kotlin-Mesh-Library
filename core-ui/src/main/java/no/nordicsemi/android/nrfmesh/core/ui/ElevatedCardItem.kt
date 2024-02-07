df658281cefb2b042a4d0ef1a83eb7fceb549193
Roshan <rajaratnam.roshan@gmail.com>
2024-02-07 08:31:02 +0100

    Implements a ElevatedCardItem with a TextField.

--- core-ui/src/main/java/no/nordicsemi/android/nrfmesh/core/ui/ElevatedCardItem.kt
+++ core-ui/src/main/java/no/nordicsemi/android/nrfmesh/core/ui/ElevatedCardItem.kt
@@ -1,16 +1,28 @@
 package no.nordicsemi.android.nrfmesh.core.ui
 
+import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Check
+import androidx.compose.material.icons.outlined.Clear
+import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
@@ -54,6 +66,85 @@ fun ElevatedCardItem(
                 horizontalArrangement = Arrangement.End
             ) {
                 it()
+            }
+        }
+    }
+}
+
+@Composable
+fun ElevatedCardItemTextField(
+    modifier: Modifier = Modifier,
+    imageVector: ImageVector,
+    title: String,
+    subtitle: String = "",
+    placeholder: String = "",
+    onValueChanged: (String) -> Unit
+){
+    var value by rememberSaveable { mutableStateOf(subtitle) }
+    var onEditClick by rememberSaveable { mutableStateOf(false) }
+    ElevatedCard(
+        modifier = modifier
+            .clickable { onEditClick = !onEditClick }
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                modifier = Modifier.padding(start = 12.dp),
+                imageVector = imageVector,
+                contentDescription = null,
+                tint = LocalContentColor.current.copy(alpha = 0.6f)
+            )
+            Crossfade(targetState = onEditClick, label = "name") { state ->
+                when (state) {
+                    true -> MeshOutlinedTextField(
+                        onFocus = onEditClick,
+                        value = value,
+                        onValueChanged = { value = it },
+                        label = { Text(text = title) },
+                        placeholder = {
+                            Text(text = placeholder)
+                        },
+                        internalTrailingIcon = {
+                            IconButton(enabled = value.isNotBlank(), onClick = { value = "" }) {
+                                Icon(imageVector = Icons.Outlined.Clear, contentDescription = null)
+                            }
+                        },
+                        content = {
+                            IconButton(
+                                modifier = Modifier.padding(horizontal = 8.dp),
+                                enabled = value.isNotBlank(),
+                                onClick = {
+                                    onEditClick = !onEditClick
+                                    value = value.trim()
+                                    onValueChanged(value)
+                                }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Outlined.Check,
+                                    contentDescription = null,
+                                )
+                            }
+                        }
+                    )
+
+                    false -> MeshTwoLineListItem(
+                        title = title,
+                        subtitle = value,
+                        trailingComposable = {
+                            IconButton(
+                                modifier = Modifier.padding(horizontal = 8.dp),
+                                onClick = {
+                                    onEditClick = !onEditClick
+                                }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Outlined.Edit,
+                                    contentDescription = null,
+                                    tint = LocalContentColor.current.copy(alpha = 0.6f)
+                                )
+                            }
+                        }
+                    )
+                }
             }
         }
     }
