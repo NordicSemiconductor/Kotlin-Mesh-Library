cdd7a3705c5ad41332078c61db2722ee36b4b255
Roshan <rajaratnam.roshan@gmail.com>
2024-02-29 09:56:37 +0100

    Add common ui component

---
+++ core-ui/src/main/java/no/nordicsemi/android/nrfmesh/core/ui/MeshLoadingItems.kt
@@ -0,0 +1,39 @@
+package no.nordicsemi.android.nrfmesh.core.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MeshLoadingItems(imageVector: ImageVector, title: String, rationale: String = "") {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Icon(
+            modifier = Modifier.size(128.dp),
+            imageVector = imageVector,
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.surfaceTint
+        )
+        Text(
+            modifier = Modifier.padding(all = 16.dp),
+            text = title
+        )
+        Text(
+            modifier = Modifier.padding(all = 16.dp),
+            text = rationale
+        )
+    }
+}
