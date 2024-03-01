6ba68d1e92e60386575306cd1eedac66915af90c
Roshan <rajaratnam.roshan@gmail.com>
2024-03-01 13:13:49 +0100

    Adds internal api to add a network key using net key configuration messages.

--- mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/model/Node.kt
+++ mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/model/Node.kt
@@ -7,13 +7,13 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
 import no.nordicsemi.kotlin.mesh.core.exception.SecurityException
 import no.nordicsemi.kotlin.mesh.core.messages.foundation.configuration.ConfigCompositionDataStatus
+import no.nordicsemi.kotlin.mesh.core.messages.foundation.configuration.ConfigNetKeyStatus
 import no.nordicsemi.kotlin.mesh.core.messages.foundation.configuration.Page0
 import no.nordicsemi.kotlin.mesh.core.model.serialization.KeySerializer
 import no.nordicsemi.kotlin.mesh.core.model.serialization.UShortAsStringSerializer
 import no.nordicsemi.kotlin.mesh.core.model.serialization.UUIDSerializer
 import no.nordicsemi.kotlin.mesh.crypto.Crypto
-import java.util.*
-import kotlin.jvm.Throws
+import java.util.UUID
 
 /**
  * The node represents a configured state of a mesh node.
@@ -346,6 +346,22 @@ data class Node internal constructor(
                 network?.updateTimestamp()
                 true
             }
+        }
+    }
+
+    /**
+     * Adds a network key to the node. Invoked only when a [ConfigNetKeyStatus] is received with a
+     * success status.
+     *
+     * @param index Network Key index.
+     */
+    internal fun add(index: KeyIndex) {
+        _netKeys.get(index) ?: _netKeys.add(NodeKey(index, false))
+        network?.let {
+            if (security is Insecure) {
+                it.networkKeys.get(index)?.lowerSecurity()
+            }
+            it.updateTimestamp()
         }
     }
 
