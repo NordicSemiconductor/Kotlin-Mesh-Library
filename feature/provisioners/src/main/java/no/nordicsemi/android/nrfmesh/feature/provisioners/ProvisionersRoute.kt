502343659dada9ae2a333afd29024c20d6d5b38d
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2024-08-28 14:39:00 +0200

    Remove unnecessary ExperimentalMaterial3Api

--- feature/provisioners/src/main/java/no/nordicsemi/android/nrfmesh/feature/provisioners/ProvisionersRoute.kt
+++ feature/provisioners/src/main/java/no/nordicsemi/android/nrfmesh/feature/provisioners/ProvisionersRoute.kt
@@ -1,4 +1,4 @@
-@file:OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class)
+@file:OptIn(ExperimentalMaterial3Api::class)
 
 package no.nordicsemi.android.nrfmesh.feature.provisioners
 
