1e27508c028f73b4ab7011db35697a8123e00623
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2024-09-06 09:54:08 +0200

    Adds support for ConfigAppKeyStatus

---
+++ mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/messages/foundation/configuration/ConfigAppKeyStatus.kt
@@ -0,0 +1,87 @@
+@file:Suppress("unused")
+
+package no.nordicsemi.kotlin.mesh.core.messages.foundation.configuration
+
+import no.nordicsemi.kotlin.data.toByteArray
+import no.nordicsemi.kotlin.mesh.core.messages.BaseMeshMessage
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigMessageInitializer
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigMessageStatus
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigNetAndAppKeyMessage
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigNetAndAppKeyMessage.Companion.decodeNetAndAppKeyIndex
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigNetKeyMessage.Companion.encodeNetKeyIndex
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigResponse
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigStatusMessage
+import no.nordicsemi.kotlin.mesh.core.model.ApplicationKey
+import no.nordicsemi.kotlin.mesh.core.model.KeyIndex
+
+/**
+ * Status declaring if the the [ConfigNetAndAppKeyMessage] operation succeeded or not.
+ *
+ * @property applicationKeyIndex Index of the application key.
+ * @property networkKeyIndex     Index of the network key.
+ * @property status              Status of the message.
+ * @property opCode              Message op code.
+ * @property parameters          Message parameters.
+ * @constructor Constructs the ConfigAppKeyStatus message.
+ */
+data class ConfigAppKeyStatus(
+    override val applicationKeyIndex: KeyIndex,
+    override val networkKeyIndex: KeyIndex,
+    override val status: ConfigMessageStatus
+) : ConfigResponse, ConfigStatusMessage, ConfigNetAndAppKeyMessage {
+
+    override val opCode = Initializer.opCode
+    override val parameters: ByteArray
+        get() = status.value.toByteArray() + encodeNetKeyIndex(networkKeyIndex)
+
+    /**
+     * Constructs the ConfigAppKeyStatus message.
+     *
+     * @param applicationKey Application key to confirm
+     * @constructor Constructs the ConfigAppKeyStatus message.
+     */
+    constructor(applicationKey: ApplicationKey) : this(
+        applicationKeyIndex = applicationKey.index,
+        networkKeyIndex = applicationKey.boundNetKeyIndex,
+        status = ConfigMessageStatus.SUCCESS
+    )
+
+    /**
+     * Constructs the ConfigAppKeyStatus message.
+     *
+     * @param request ConfigNetKeyMessage operation that was sent to the mesh node.
+     * @param status  Status of the message.
+     * @constructor Constructs the ConfigAppKeyStatus message.
+     */
+    constructor(
+        request: ConfigAppKeyAdd,
+        status: ConfigMessageStatus
+    ) : this(
+        applicationKeyIndex = request.applicationKeyIndex,
+        networkKeyIndex = request.networkKeyIndex,
+        status = status
+    )
+
+    companion object Initializer : ConfigMessageInitializer {
+        override val opCode = 0x8003u
+
+        /**
+         * Initializes the ConfigAppKeyStatus message.
+         *
+         * @param parameters Message parameters.
+         * @return ConfigAppKeyStatus or null if the parameters are invalid.
+         */
+        override fun init(parameters: ByteArray?): BaseMeshMessage? = parameters?.takeIf {
+            it.size == 4
+        }?.let { params ->
+            ConfigMessageStatus.from(params.first().toUByte())?.let {
+                val decodedNetAndAppKeyIndex = decodeNetAndAppKeyIndex(data = params, offset = 1)
+                ConfigAppKeyStatus(
+                    applicationKeyIndex = decodedNetAndAppKeyIndex.applicationKeyIndex,
+                    networkKeyIndex = decodedNetAndAppKeyIndex.networkKeyIndex,
+                    status = it
+                )
+            }
+        }
+    }
+}
