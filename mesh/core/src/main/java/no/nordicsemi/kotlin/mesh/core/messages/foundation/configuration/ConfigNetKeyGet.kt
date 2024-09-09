f0fc35b88f5da987959c918e029590f01d429011
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2024-09-09 20:02:15 +0200

    Implements ConfigNetKeyGet and ConfigNetKeyList messages.

---
+++ mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/messages/foundation/configuration/ConfigNetKeyGet.kt
@@ -0,0 +1,35 @@
+@file:Suppress("unused")
+
+package no.nordicsemi.kotlin.mesh.core.messages.foundation.configuration
+
+import no.nordicsemi.kotlin.mesh.core.messages.AcknowledgedConfigMessage
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigMessageInitializer
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigNetKeyMessage
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigNetKeyMessage.Companion.decodeNetKeyIndex
+import no.nordicsemi.kotlin.mesh.core.model.KeyIndex
+
+/**
+ * This message is used to get a network key from the mesh network.
+ *
+ * @property opCode               Message op code.
+ * @property parameters           Message parameters.
+ * @property responseOpCode       Op Code of the response message.
+ * @constructor Constructs the ConfigNetKeyGet message.
+ */
+class ConfigNetKeyGet : AcknowledgedConfigMessage {
+    override val opCode: UInt = Initializer.opCode
+    override val parameters: ByteArray? = null
+
+    override val responseOpCode = ConfigNetKeyStatus.opCode
+
+    override fun toString() = "ConfigNetKeyGet(opCode: $opCode)"
+
+    companion object Initializer : ConfigMessageInitializer {
+        override val opCode = 0x8042u
+        override fun init(parameters: ByteArray?) = parameters?.takeIf {
+            it.isEmpty()
+        }?.let {
+            ConfigNetKeyGet()
+        } ?: ConfigNetKeyGet()
+    }
+}
