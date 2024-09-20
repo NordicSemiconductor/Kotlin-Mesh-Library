08a5a38bdadd9ca273445296eced0fc31fb48132
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2024-09-20 03:44:27 +0200

    Adds support for ConfigFriendStatus message

---
+++ mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/messages/foundation/configuration/ConfigFriendStatus.kt
@@ -0,0 +1,32 @@
+@file:Suppress("unused")
+
+package no.nordicsemi.kotlin.mesh.core.messages.foundation.configuration
+
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigMessageInitializer
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigResponse
+import no.nordicsemi.kotlin.mesh.core.model.FeatureState
+import no.nordicsemi.kotlin.mesh.core.model.Node
+
+/**
+ * ConfigFriendStatus is the response received/sent for [ConfigFriendGet] message.
+ *
+ * @property state The state of the Friend feature.
+ */
+data class ConfigFriendStatus(val state: FeatureState) : ConfigResponse {
+    override val opCode = Initializer.opCode
+    override val parameters: ByteArray = byteArrayOf(state.value.toByte())
+
+    constructor(node: Node) : this(node.features.friend?.state ?: FeatureState.Unsupported)
+
+    override fun toString() = "ConfigFriendStatus(opCode: $opCode, state: $state)"
+
+    companion object Initializer : ConfigMessageInitializer {
+        override val opCode = 0x8011u
+
+        override fun init(parameters: ByteArray?) = parameters?.takeIf {
+            it.size == 1
+        }?.let {
+            ConfigFriendStatus(state = FeatureState.from(it[0].toUInt().toInt()))
+        }
+    }
+}
