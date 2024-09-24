df74de009cb21cab730289fa0125c4764d0e3568
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2024-09-24 20:09:03 +0200

    Bump dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,9 +8,9 @@ pluginManagement {
 
     plugins {
         id("org.jetbrains.dokka") version "1.9.20"
-        id("org.jetbrains.kotlin.jvm") version "2.0.0"
+        id("org.jetbrains.kotlin.jvm") version "2.0.20"
         id("com.android.library") version "8.5.2"
-        id("org.jetbrains.kotlin.android") version "2.0.0"
+        id("org.jetbrains.kotlin.android") version "2.0.20"
         // id("org.jetbrains.kotlin.android") version "2.0.0"
     }
 }
