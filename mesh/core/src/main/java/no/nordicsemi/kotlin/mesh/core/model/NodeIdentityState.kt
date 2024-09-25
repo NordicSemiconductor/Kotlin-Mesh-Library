ea47ca35de3fdf8fb9dd0cb8c1b92647ab8d01f3
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2024-09-25 12:33:16 +0200

    Implements NodeIdentityState

---
+++ mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/model/NodeIdentityState.kt
@@ -0,0 +1,28 @@
+package no.nordicsemi.kotlin.mesh.core.model
+
+/**
+ * Node Identity state
+ *
+ * @property value               Value of the state.
+ * @property STOPPED             Advertising with Node Identity for a subnet is stopped.
+ * @property RUNNING             Advertising with Node Identity for a subnet is running.
+ * @property NOT_SUPPORTED       Advertising with Node Identity for a subnet is not supported.
+ */
+enum class NodeIdentityState(val value: UByte) {
+    STOPPED(0x00u),
+    RUNNING(0x01u),
+    NOT_SUPPORTED(0x02u);
+
+    val isSupported: Boolean
+        get() = this != NOT_SUPPORTED
+
+    companion object {
+
+        /**
+         * Returns the [NodeIdentityState] for the given value.
+         *
+         * @param value Value of the state.
+         */
+        fun from(value: UByte) = entries.first { it.value == value }
+    }
+}
