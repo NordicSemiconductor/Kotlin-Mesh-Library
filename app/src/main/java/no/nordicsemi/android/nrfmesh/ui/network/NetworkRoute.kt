5c31cf958531510230ee2d1c8ad7ae73e9c8cb09
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2024-11-05 10:07:44 +0100

    Clean up

--- app/src/main/java/no/nordicsemi/android/nrfmesh/ui/network/NetworkRoute.kt
+++ app/src/main/java/no/nordicsemi/android/nrfmesh/ui/network/NetworkRoute.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
@@ -179,7 +180,7 @@ fun BottomNavigationBar(
                 },
                 icon = {
                     Icon(
-                        if (selectedItem == index) {
+                        imageVector = if (selectedItem == index) {
                             destination.selectedIcon
                         } else {
                             destination.unselectedIcon
