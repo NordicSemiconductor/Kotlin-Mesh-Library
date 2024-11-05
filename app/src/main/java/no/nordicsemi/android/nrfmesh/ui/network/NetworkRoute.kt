73c0bd796c1313530fb6f4f2b876e839e22d31e1
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2024-11-05 10:13:28 +0100

    Clean up

--- app/src/main/java/no/nordicsemi/android/nrfmesh/ui/network/NetworkRoute.kt
+++ app/src/main/java/no/nordicsemi/android/nrfmesh/ui/network/NetworkRoute.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
