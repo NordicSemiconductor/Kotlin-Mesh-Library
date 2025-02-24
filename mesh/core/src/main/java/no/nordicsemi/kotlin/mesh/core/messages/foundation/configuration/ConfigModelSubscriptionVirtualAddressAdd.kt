f8e072a210a37790d6144e2a11eb194c0393ec9a
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2025-02-24 14:32:48 +0100

    Adds support for ConfigModelSubscriptionVirtual messages

---
+++ mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/messages/foundation/configuration/ConfigModelSubscriptionVirtualAddressAdd.kt
@@ -0,0 +1,92 @@
+package no.nordicsemi.kotlin.mesh.core.messages.foundation.configuration
+
+import no.nordicsemi.kotlin.data.getUShort
+import no.nordicsemi.kotlin.data.toByteArray
+import no.nordicsemi.kotlin.mesh.core.messages.AcknowledgedConfigMessage
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigAnyModelMessage
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigMessageInitializer
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigVirtualLabelMessage
+import no.nordicsemi.kotlin.mesh.core.model.Group
+import no.nordicsemi.kotlin.mesh.core.model.Model
+import no.nordicsemi.kotlin.mesh.core.model.SigModelId
+import no.nordicsemi.kotlin.mesh.core.model.UnicastAddress
+import no.nordicsemi.kotlin.mesh.core.model.VendorModelId
+import no.nordicsemi.kotlin.mesh.core.model.VirtualAddress
+import java.nio.ByteOrder
+import java.util.UUID
+
+/**
+ * This message is used to add a virtual address to the model subscriptions list.
+ *
+ * @property virtualLabel      Virtual address of the group.
+ * @property elementAddress    Element address of the model.
+ * @property modelIdentifier   Model identifier.
+ * @property companyIdentifier Company identifier, if the model is a vendor model.
+ */
+@Suppress("unused")
+class ConfigModelSubscriptionVirtualAddressAdd(
+    override val elementAddress: UnicastAddress,
+    override val virtualLabel: UUID,
+    override val modelIdentifier: UShort,
+    override val companyIdentifier: UShort?,
+) : AcknowledgedConfigMessage, ConfigVirtualLabelMessage, ConfigAnyModelMessage {
+    override val opCode = Initializer.opCode
+    override val responseOpCode = ConfigModelSubscriptionStatus.opCode
+    override val parameters: ByteArray
+        get() {
+            val data = byteArrayOf() +
+                    virtualLabel.toByteArray(order = ByteOrder.LITTLE_ENDIAN) +
+                    elementAddress.address.toByteArray(order = ByteOrder.LITTLE_ENDIAN) +
+                    virtualLabel.toByteArray(order = ByteOrder.LITTLE_ENDIAN)
+
+            return data.plus(elements = companyIdentifier?.let {
+                modelIdentifier.toByteArray(order = ByteOrder.LITTLE_ENDIAN) +
+                        it.toByteArray(order = ByteOrder.LITTLE_ENDIAN)
+            } ?: modelIdentifier.toByteArray(order = ByteOrder.LITTLE_ENDIAN))
+        }
+
+    /**
+     * Convenience constructor to create a ConfigModelSubscriptionAdd message.
+     *
+     * @param group Group to add the model subscription to.
+     * @param model Model to add the subscription to.
+     * @throws IllegalArgumentException If the model does not have a parent element.
+     */
+    @Throws(IllegalArgumentException::class)
+    constructor(group: Group, model: Model) : this(
+        virtualLabel = (group.address as? VirtualAddress)?.uuid
+            ?: throw IllegalArgumentException("Group address must be a virtual address"),
+        elementAddress = model.parentElement?.unicastAddress
+            ?: throw IllegalArgumentException("Element address cannot be null"),
+        modelIdentifier = when (model.modelId) {
+            is SigModelId -> model.modelId.modelIdentifier
+            is VendorModelId -> model.modelId.modelIdentifier
+        },
+        companyIdentifier = (model.modelId as? VendorModelId)?.companyIdentifier,
+    )
+
+    companion object Initializer : ConfigMessageInitializer {
+        override val opCode = 0x8020u
+
+        override fun init(parameters: ByteArray?) = parameters?.takeIf {
+            it.size == 20 || it.size == 22
+        }?.let { params ->
+            ConfigModelSubscriptionVirtualAddressAdd(
+                elementAddress = UnicastAddress(
+                    address = params.getUShort(offset = 0, order = ByteOrder.LITTLE_ENDIAN)
+                ),
+                virtualLabel = UUID.nameUUIDFromBytes(
+                    params.copyOfRange(fromIndex = 2, toIndex = 18)
+                ),
+                companyIdentifier = if (params.size == 24) params.getUShort(
+                    offset = 18,
+                    order = ByteOrder.LITTLE_ENDIAN
+                ) else null,
+                modelIdentifier = if (params.size == 24) params.getUShort(
+                    offset = 20,
+                    order = ByteOrder.LITTLE_ENDIAN
+                ) else params.getUShort(offset = 18, order = ByteOrder.LITTLE_ENDIAN)
+            )
+        }
+    }
+}
