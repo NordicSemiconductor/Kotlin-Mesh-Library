e107b393228b09a460decddd9798b53e8cd9e751
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2025-03-11 11:02:11 +0100

    Addresses for subscriptions

--- core/common/src/main/java/no/nordicsemi/android/nrfmesh/core/common/AddressDescriptions.kt
+++ core/common/src/main/java/no/nordicsemi/android/nrfmesh/core/common/AddressDescriptions.kt
@@ -6,13 +6,12 @@ import no.nordicsemi.kotlin.mesh.core.model.AllProxies
 import no.nordicsemi.kotlin.mesh.core.model.AllRelays
 import no.nordicsemi.kotlin.mesh.core.model.FixedGroupAddress
 
-
 val fixedGroupAddresses = listOf(AllRelays, AllFriends, AllProxies, AllNodes)
 
 /**
  * Returns a human readable description of the [FixedGroupAddress].
  */
-fun FixedGroupAddress.description() = when(this){
+fun FixedGroupAddress.description() = when (this) {
     is AllProxies -> "All Proxies"
     is AllNodes -> "All Nodes"
     is AllRelays -> "All Relays"
