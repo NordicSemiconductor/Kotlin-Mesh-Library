6fec0a7591f5dd0492f495128f909885d1119922
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2025-03-11 09:32:16 +0100

    Move fixed group addresses list to commons

---
+++ core/common/src/main/java/no/nordicsemi/android/nrfmesh/core/common/AddressDescriptions.kt
@@ -0,0 +1,20 @@
+package no.nordicsemi.android.nrfmesh.core.common
+
+import no.nordicsemi.kotlin.mesh.core.model.AllFriends
+import no.nordicsemi.kotlin.mesh.core.model.AllNodes
+import no.nordicsemi.kotlin.mesh.core.model.AllProxies
+import no.nordicsemi.kotlin.mesh.core.model.AllRelays
+import no.nordicsemi.kotlin.mesh.core.model.FixedGroupAddress
+
+
+val fixedGroupAddresses = listOf(AllRelays, AllFriends, AllProxies, AllNodes)
+
+/**
+ * Returns a human readable description of the [FixedGroupAddress].
+ */
+fun FixedGroupAddress.description() = when(this){
+    is AllProxies -> "All Proxies"
+    is AllNodes -> "All Nodes"
+    is AllRelays -> "All Relays"
+    is AllFriends -> "All Friends"
+}
