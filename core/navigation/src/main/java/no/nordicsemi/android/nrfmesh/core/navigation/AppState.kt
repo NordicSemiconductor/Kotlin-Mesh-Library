00c8054348158c709b330b8603ae23c42b638e43
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2025-03-14 10:23:29 +0100

    Clean up

--- core/navigation/src/main/java/no/nordicsemi/android/nrfmesh/core/navigation/AppState.kt
+++ core/navigation/src/main/java/no/nordicsemi/android/nrfmesh/core/navigation/AppState.kt
@@ -3,19 +3,15 @@ package no.nordicsemi.android.nrfmesh.core.navigation
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Stable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
 
 /**
  * AppState is a class that holds the current state of the application.
  *
+ * @property navController          The [NavHostController] that will be used to navigate between
  * @property snackbarHostState      The [SnackbarHostState] that will be used to show snackbars.
  *                                  destinations of the application.
- * @property currentScreen          Current screen that is displayed.
- *                                  clicked.
  * @property windowSizeClass        The current window size class.
  */
 @Stable
@@ -24,9 +20,6 @@ abstract class AppState(
     val snackbarHostState: SnackbarHostState,
     val windowSizeClass: WindowSizeClass,
 ) {
-    var currentScreen by mutableStateOf<Screen?>(null)
-        protected set
-
     val previousBackStackEntry: NavBackStackEntry?
         get() = navController.previousBackStackEntry
 
