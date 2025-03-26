72efa896bb1303009941b583f389db27e60cafdf
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2025-03-27 00:10:51 +0100

    Fix dependencies

--- feature/mesh-bearer-android/build.gradle.kts
+++ feature/mesh-bearer-android/build.gradle.kts
@@ -13,8 +13,8 @@ dependencies {
     implementation(libs.androidx.core)
     implementation(libs.nordic.blek.core)
     implementation(libs.nordic.blek.client)
+    implementation(libs.nordic.kotlin.data)
 
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
 }
