8dea6d786dff9f17b2126c0582fcf8960b12c32e
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2025-05-22 10:03:35 +0200

    Implements ConfigLowPowerNodePollTimeoutGet and ConfigLowPowerNodePollTimeoutStatus messages

---
+++ mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/messages/foundation/configuration/ConfigLowPowerNodePollTimeoutGet.kt
@@ -0,0 +1,64 @@
+@file:Suppress("unused")
+
+package no.nordicsemi.kotlin.mesh.core.messages.foundation.configuration
+
+import no.nordicsemi.kotlin.data.getUShort
+import no.nordicsemi.kotlin.data.toByteArray
+import no.nordicsemi.kotlin.mesh.core.messages.AcknowledgedConfigMessage
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigMessageInitializer
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigNetKeyMessage
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigNetKeyMessage.Companion.decodeNetKeyIndex
+import no.nordicsemi.kotlin.mesh.core.model.Address
+import no.nordicsemi.kotlin.mesh.core.model.KeyIndex
+import no.nordicsemi.kotlin.mesh.core.model.KeyRefreshPhase
+import no.nordicsemi.kotlin.mesh.core.model.KeyRefreshPhaseTransition
+import no.nordicsemi.kotlin.mesh.core.model.MeshAddress
+import no.nordicsemi.kotlin.mesh.core.model.NetworkKey
+import java.nio.ByteOrder
+
+/**
+ * The Config Low Power Node PollTimeout Get is an acknowledged message used to get the current
+ * value of PollTimeout timer of the Low Power node within a Friend Node. The message is sent to a
+ * Friend Node that has claimed to be handling messages by sending ACKs On Behalf Of (OBO) the
+ * indicated Low Power Node. This message should only be sent to a Node that has the Friend feature
+ * supported and enabled.
+ *
+ * @property address The address of the Low Power node.
+ */
+class ConfigLowPowerNodePollTimeoutGet(val address: Address) : AcknowledgedConfigMessage {
+    override val opCode: UInt = Initializer.opCode
+    override val responseOpCode = ConfigLowPowerNodePollTimeoutStatus.opCode
+    override val parameters: ByteArray = address.toByteArray(order = ByteOrder.LITTLE_ENDIAN)
+
+    /**
+     * Constructs a [ConfigLowPowerNodePollTimeoutGet] message for a given low power node address.
+     *
+     * @param address The address of the Low Power node.
+     */
+    constructor(address: MeshAddress) : this(address = address.address)
+
+    @OptIn(ExperimentalStdlibApi::class)
+    override fun toString() = "ConfigLowPowerNodePollTimeoutGet(opCode: " +
+            "0x${opCode.toHexString()}, address: ${address.toHexString()})"
+
+    companion object Initializer : ConfigMessageInitializer {
+        override val opCode = 0x802Du
+
+        /**
+         * Initializes the [ConfigLowPowerNodePollTimeoutGet] based on the given parameters.
+         *
+         * @param parameters Message parameters.
+         * @return ConfigKeyRefreshPhaseGet or null if the parameters are invalid.
+         */
+        override fun init(parameters: ByteArray?) = parameters
+            ?.takeIf { it.size == 2 }
+            ?.let {
+                ConfigLowPowerNodePollTimeoutGet(
+                    address = it.getUShort(
+                        offset = 0,
+                        order = ByteOrder.LITTLE_ENDIAN
+                    )
+                )
+            }
+    }
+}
---
+++ mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/messages/foundation/configuration/ConfigLowPowerNodePollTimeoutStatus.kt
@@ -0,0 +1,106 @@
+@file:Suppress("unused")
+
+package no.nordicsemi.kotlin.mesh.core.messages.foundation.configuration
+
+import no.nordicsemi.kotlin.data.getUInt
+import no.nordicsemi.kotlin.data.getUShort
+import no.nordicsemi.kotlin.data.toByteArray
+import no.nordicsemi.kotlin.mesh.core.messages.AcknowledgedConfigMessage
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigMessageInitializer
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigNetKeyMessage
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigNetKeyMessage.Companion.decodeNetKeyIndex
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigResponse
+import no.nordicsemi.kotlin.mesh.core.model.Address
+import no.nordicsemi.kotlin.mesh.core.model.KeyIndex
+import no.nordicsemi.kotlin.mesh.core.model.KeyRefreshPhase
+import no.nordicsemi.kotlin.mesh.core.model.KeyRefreshPhaseTransition
+import no.nordicsemi.kotlin.mesh.core.model.MeshAddress
+import no.nordicsemi.kotlin.mesh.core.model.NetworkKey
+import no.nordicsemi.kotlin.mesh.core.model.UnicastAddress
+import java.nio.ByteOrder
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+
+/**
+ * The Config Low Power Node PollTimeout Status is an unacknowledged message used to report the
+ * current value of the PollTimeout timer of the Low Power Node within a Friend Node.
+ *
+ * @property address              Address of the Low Power node.
+ * @property pollTimeout          Poll timeout value.
+ * @property pollTimeOutInterval  Poll time out interval. This is 24-bit value, where:
+ *                                0x000000 -            The Node is no longer a Friend node of the
+ *                                                      Low Power Node identified by the LPNAddress.
+ *                                0x000001 - 0x000009 - Prohibited.
+ *                                0x00000A - 0x34BBFF - The PollTimeout timer value in units of  100
+ *                                                      milliseconds, which represents a range from
+ *                                                      1 second to 3 days 23 hours 59 seconds 900
+ *                                                      milliseconds.
+ *                                0x34BC00 - 0xFFFFFF - Prohibited.
+ */
+class ConfigLowPowerNodePollTimeoutStatus(
+    val address: Address,
+    val pollTimeout: UInt,
+) : ConfigResponse {
+    override val opCode: UInt = Initializer.opCode
+    override val parameters: ByteArray = address.toByteArray(order = ByteOrder.LITTLE_ENDIAN)
+
+    val pollTimeOutInterval: Duration?
+        get() = when {
+            pollTimeout.toLong() in 0x00000A..0x34BBFF ->
+                (pollTimeout.toLong() * 100L).milliseconds
+            else -> null
+        }
+
+    /**
+     * Constructs a [ConfigLowPowerNodePollTimeoutStatus] message for a given low power node address.
+     *
+     * @param address     The address of the Low Power node.
+     * @param pollTimeout The PollTimeout value.
+     */
+    constructor(address: UnicastAddress, pollTimeout: UInt) : this(
+        address = address.address,
+        pollTimeout = pollTimeout
+    )
+
+    /**
+     * Constructs a [ConfigLowPowerNodePollTimeoutStatus] message for a given low power node
+     * address.
+     *
+     * @param request The [ConfigLowPowerNodePollTimeoutGet] request.
+     */
+    constructor(request: ConfigLowPowerNodePollTimeoutGet) : this(
+        address = request.address,
+        pollTimeout = 0u
+    )
+
+    @OptIn(ExperimentalStdlibApi::class)
+    override fun toString() = "ConfigLowPowerNodePollTimeoutStatus(opCode: " +
+            "0x${opCode.toHexString()}, address: ${address.toHexString()})"
+
+    companion object Initializer : ConfigMessageInitializer {
+        override val opCode = 0x802Eu
+
+        /**
+         * Initializes the [ConfigLowPowerNodePollTimeoutStatus] based on the given parameters.
+         *
+         * @param parameters Message parameters.
+         * @return ConfigLowPowerNodePollTimeoutStatus or null if the parameters are invalid.
+         */
+        override fun init(parameters: ByteArray?) = parameters
+            ?.takeIf { it.size == 5 }
+            ?.let {
+                ConfigLowPowerNodePollTimeoutStatus(
+                    address = it.getUShort(
+                        offset = 0,
+                        order = ByteOrder.LITTLE_ENDIAN
+                    ),
+                    // Extend the parameters by 1 byte and read pollTimeout as UInt.
+                    // The parameters on Access Layer are encoded using Little Endian.
+                    pollTimeout = (it + byteArrayOf(0)).getUInt(
+                        offset = 2,
+                        order = ByteOrder.LITTLE_ENDIAN
+                    )
+                )
+            }
+    }
+}
