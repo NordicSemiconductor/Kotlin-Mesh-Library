0cd66096071060e1e037547a3dc6a43dab263cc3
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2025-05-22 10:05:07 +0200

    Implements ConfigVendorModelAppGet and ConfigVendorModelAppList messages

---
+++ mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/messages/foundation/configuration/ConfigVendorModelAppGet.kt
@@ -0,0 +1,45 @@
+package no.nordicsemi.kotlin.mesh.core.messages.foundation.configuration
+
+import no.nordicsemi.kotlin.data.getUShort
+import no.nordicsemi.kotlin.data.toByteArray
+import no.nordicsemi.kotlin.mesh.core.messages.AcknowledgedConfigMessage
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigMessageInitializer
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigModelMessage
+import no.nordicsemi.kotlin.mesh.core.model.SigModelId
+import no.nordicsemi.kotlin.mesh.core.model.UnicastAddress
+import no.nordicsemi.kotlin.mesh.core.model.VendorModelId
+import java.nio.ByteOrder
+
+/**
+ * This message is used to get the list of Application Keys bound to a Vendor Model.
+ */
+class ConfigVendorModelAppGet(
+    override val modelId: VendorModelId,
+    override val elementAddress: UnicastAddress,
+) : AcknowledgedConfigMessage, ConfigModelMessage {
+    override val opCode: UInt = Initializer.opCode
+    override val responseOpCode: UInt = ConfigVendorModelAppList.opCode
+    override val modelIdentifier: UShort = modelId.modelIdentifier
+    override val parameters: ByteArray
+        get() = elementAddress.address.toByteArray(order = ByteOrder.LITTLE_ENDIAN) +
+                modelIdentifier.toByteArray(order = ByteOrder.LITTLE_ENDIAN)
+
+    override fun toString() = "ConfigVendorModelAppGet(elementAddress: $elementAddress, " +
+            "modelId: $modelId)"
+
+    companion object Initializer : ConfigMessageInitializer {
+        override val opCode: UInt = 0x804Du
+
+        override fun init(parameters: ByteArray?) = parameters?.takeIf {
+            it.size == 6
+        }?.let { params ->
+            ConfigVendorModelAppGet(
+                elementAddress = UnicastAddress(params.getUShort(0, ByteOrder.LITTLE_ENDIAN)),
+                modelId = VendorModelId(
+                    companyIdentifier = params.getUShort(2, ByteOrder.LITTLE_ENDIAN),
+                    modelIdentifier = params.getUShort(4, ByteOrder.LITTLE_ENDIAN)
+                )
+            )
+        }
+    }
+}
---
+++ mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/messages/foundation/configuration/ConfigVendorModelAppList.kt
@@ -0,0 +1,105 @@
+package no.nordicsemi.kotlin.mesh.core.messages.foundation.configuration
+
+import no.nordicsemi.kotlin.data.getUShort
+import no.nordicsemi.kotlin.data.toByteArray
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigMessage
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigMessage.ConfigMessageUtils.decode
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigMessage.ConfigMessageUtils.encode
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigMessageInitializer
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigMessageStatus
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigModelAppList
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigResponse
+import no.nordicsemi.kotlin.mesh.core.messages.ConfigStatusMessage
+import no.nordicsemi.kotlin.mesh.core.model.ApplicationKey
+import no.nordicsemi.kotlin.mesh.core.model.KeyIndex
+import no.nordicsemi.kotlin.mesh.core.model.SigModelId
+import no.nordicsemi.kotlin.mesh.core.model.UnicastAddress
+import no.nordicsemi.kotlin.mesh.core.model.VendorModelId
+import org.intellij.lang.annotations.Identifier
+import java.nio.ByteOrder
+
+/**
+ * This message is the status message to the [ConfigVendorModelAppGet] message. This lists the
+ * Application Keys bound to a Vendor Model.
+ */
+class ConfigVendorModelAppList(
+    override val status: ConfigMessageStatus,
+    override val elementAddress: UnicastAddress,
+    override val applicationKeyIndexes: List<KeyIndex>,
+    override val modelId: VendorModelId,
+) : ConfigResponse, ConfigStatusMessage, ConfigModelAppList {
+    override val opCode: UInt = Initializer.opCode
+    override val modelIdentifier: UShort = modelId.modelIdentifier
+    override val parameters: ByteArray?
+        get() = byteArrayOf(status.value.toByte()) +
+                elementAddress.address.toByteArray(order = ByteOrder.LITTLE_ENDIAN) +
+                modelId.modelIdentifier.toByteArray(order = ByteOrder.LITTLE_ENDIAN) +
+                modelId.companyIdentifier.toByteArray(order = ByteOrder.LITTLE_ENDIAN) +
+                encode(indexes = applicationKeyIndexes.toTypedArray())
+
+    /**
+     * Convenience constructor to create the ConfigSigModelAppList message.
+     *
+     * @param request [ConfigSigModelAppGet] message that this is a response to.
+     * @param keys    List of application keys bound to the model.
+     */
+    constructor(request: ConfigVendorModelAppGet, keys: List<ApplicationKey>) : this(
+        status = ConfigMessageStatus.SUCCESS,
+        modelId = request.modelId,
+        elementAddress = request.elementAddress,
+        applicationKeyIndexes = keys.map { it.index }
+    )
+
+    /**
+     * Convenience constructor to create the ConfigSigModelAppList message.
+     *
+     * @param request [ConfigSigModelAppGet] message that this is a response to.
+     * @param status  Status of the request.
+     */
+    constructor(request: ConfigVendorModelAppGet, status: ConfigMessageStatus) : this(
+        status = status,
+        modelId = request.modelId,
+        elementAddress = request.elementAddress,
+        applicationKeyIndexes = listOf()
+    )
+
+
+    override fun toString() = "ConfigVendorModelAppList(" +
+            "applicationKeyIndexes: $applicationKeyIndexes, " +
+            "elementAddress: $elementAddress, " +
+            "modelId: $modelId)"
+
+    companion object Initializer : ConfigMessageInitializer {
+        override val opCode: UInt = 0x804Eu
+
+        override fun init(parameters: ByteArray?) = parameters
+            ?.takeIf { it.size >= 7 }
+            ?.let { params ->
+                ConfigMessageStatus.from(params[0].toUByte())?.let { status ->
+                    ConfigVendorModelAppList(
+                        status = status,
+                        elementAddress = UnicastAddress(
+                            address = params.getUShort(
+                                offset = 1,
+                                order = ByteOrder.LITTLE_ENDIAN
+                            )
+                        ),
+                        modelId = VendorModelId(
+                            companyIdentifier = params.getUShort(
+                                offset = 3,
+                                order = ByteOrder.LITTLE_ENDIAN
+                            ),
+                            modelIdentifier = params.getUShort(
+                                offset = 5,
+                                order = ByteOrder.LITTLE_ENDIAN
+                            )
+                        ),
+                        applicationKeyIndexes = decode(
+                            data = params,
+                            offset = 7
+                        ).toList()
+                    )
+                }
+            }
+    }
+}
