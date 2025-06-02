4ffa11ba30af0bd8208c536d75a4b96b7094c568
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2025-06-02 09:56:17 +0200

    Clean up

--- mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/layers/access/AccessLayer.kt
+++ mesh/core/src/main/java/no/nordicsemi/kotlin/mesh/core/layers/access/AccessLayer.kt
@@ -500,12 +500,8 @@ internal class AccessLayer(private val networkManager: NetworkManager) : AutoClo
         } else {
             // .. otherwise, the Device Key was used.
             val models = localNode.elements
-                .flatMap {
-                    it.models.filter { model ->
-                        model.supportsDeviceKey
-                    }
-                }
-            //.filter { it.supportsDeviceKey }
+                .flatMap { it.models }
+                .filter { it.supportsDeviceKey }
 
             for (model in models) {
                 val eventHandler = model.eventHandler ?: continue
