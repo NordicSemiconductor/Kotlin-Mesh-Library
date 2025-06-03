005f28c05441a06efcf0ae762876c4d8e060248a
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2025-06-03 11:22:32 +0200

    Adds a workaround to get notifications working by requesting MTU first

--- core/data/src/main/java/no/nordicsemi/android/nrfmesh/core/data/bearer/AndroidGattBearer.kt
+++ core/data/src/main/java/no/nordicsemi/android/nrfmesh/core/data/bearer/AndroidGattBearer.kt
@@ -1,12 +1,14 @@
 package no.nordicsemi.android.nrfmesh.core.data.bearer
 
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.first
 import no.nordicsemi.android.nrfmesh.core.common.di.DefaultDispatcher
 import no.nordicsemi.kotlin.ble.client.android.CentralManager
 import no.nordicsemi.kotlin.ble.client.android.ConjunctionFilterScope
 import no.nordicsemi.kotlin.ble.client.android.Peripheral
 import no.nordicsemi.kotlin.ble.client.android.ScanResult
 import no.nordicsemi.kotlin.mesh.bearer.gatt.GattBearerImpl
+import kotlin.uuid.ExperimentalUuidApi
 
 class AndroidGattBearer(
     @DefaultDispatcher dispatcher: CoroutineDispatcher,
@@ -23,9 +25,15 @@ class AndroidGattBearer(
     centralManager = centralManager,
     peripheral = peripheral
 ) {
+    @OptIn(ExperimentalUuidApi::class)
     override suspend fun open() {
         super.open()
         // Request the maximum transmission unit (MTU) size.
         peripheral.requestHighestValueLength()
+
+        // Start observing the discovered services
+        val services = peripheral.services()
+            .first { it.isNotEmpty() }
+        configureGatt(services = services)
     }
 }
--- core/data/src/main/java/no/nordicsemi/android/nrfmesh/core/data/bearer/AndroidPbGattBearer.kt
+++ core/data/src/main/java/no/nordicsemi/android/nrfmesh/core/data/bearer/AndroidPbGattBearer.kt
@@ -1,6 +1,7 @@
 package no.nordicsemi.android.nrfmesh.core.data.bearer
 
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.first
 import no.nordicsemi.android.nrfmesh.core.common.di.DefaultDispatcher
 import no.nordicsemi.kotlin.ble.client.android.CentralManager
 import no.nordicsemi.kotlin.ble.client.android.ConjunctionFilterScope
@@ -8,6 +9,7 @@ import no.nordicsemi.kotlin.ble.client.android.Peripheral
 import no.nordicsemi.kotlin.ble.client.android.ScanResult
 import no.nordicsemi.kotlin.mesh.bearer.gatt.GattBearerImpl
 import no.nordicsemi.kotlin.mesh.bearer.pbgatt.PbGattBearerImpl
+import kotlin.uuid.ExperimentalUuidApi
 
 class AndroidPbGattBearer(
     @DefaultDispatcher dispatcher: CoroutineDispatcher,
@@ -24,9 +26,15 @@ class AndroidPbGattBearer(
     centralManager = centralManager,
     peripheral = peripheral
 ) {
+    @OptIn(ExperimentalUuidApi::class)
     override suspend fun open() {
         super.open()
         // Request the maximum transmission unit (MTU) size.
         peripheral.requestHighestValueLength()
+
+        // Start observing the discovered services
+        val services = peripheral.services()
+            .first { it.isNotEmpty() }
+        configureGatt(services = services)
     }
 }
