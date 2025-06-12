6906d71de502424f460c7ad2a9133d135333a8eb
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2025-06-12 12:15:00 +0200

    Improvements

--- feature/settings/src/main/java/no/nordicsemi/android/nrfmesh/feature/settings/SettingsViewModel.kt
+++ feature/settings/src/main/java/no/nordicsemi/android/nrfmesh/feature/settings/SettingsViewModel.kt
@@ -8,6 +8,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import no.nordicsemi.android.nrfmesh.core.data.CoreDataRepository
 import no.nordicsemi.android.nrfmesh.core.navigation.ClickableSetting
@@ -29,22 +30,31 @@ class SettingsViewModel @Inject constructor(
         _uiState.value = _uiState.value.copy(
             selectedSetting = savedStateHandle.toRoute<SettingsRoute>().selectedSetting
         )
+        observeNetworkState()
+    }
 
-        viewModelScope.launch {
-            repository.network.collect {
-                val selectedSetting = _uiState.value.selectedSetting
-                _uiState.value = _uiState.value.copy(
-                    networkState = MeshNetworkState.Success(
-                        network = it,
-                        settingsListData = SettingsListData(it)
-                    ),
-                    selectedSetting = selectedSetting
-                )
-                network = it
-            }
+    /**
+     * Observes the network state and updates the UI state with the current network data.
+     */
+    private fun observeNetworkState() {
+        repository.network.onEach {
+            val selectedSetting = _uiState.value.selectedSetting
+            _uiState.value = _uiState.value.copy(
+                networkState = MeshNetworkState.Success(
+                    network = it,
+                    settingsListData = SettingsListData(it)
+                ),
+                selectedSetting = selectedSetting
+            )
+            network = it
         }
     }
 
+    /**
+     * Invoked when a setting is selected.
+     *
+     * @param clickableSetting The setting that was clicked.
+     */
     internal fun onItemSelected(clickableSetting: ClickableSetting) {
         _uiState.value = _uiState.value.copy(selectedSetting = clickableSetting)
     }
@@ -59,6 +69,9 @@ class SettingsViewModel @Inject constructor(
         save()
     }
 
+    /**
+     * Moves the provisioner to a new index in the list.
+     */
     fun moveProvisioner(provisioner: Provisioner, newIndex: Int) {
         viewModelScope.launch {
             network.move(provisioner, newIndex)
