973a81e4a1bae7d9bd0e2d103d29e7c5e3d6499f
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2025-06-12 12:16:08 +0200

    Add IV Index module

---
+++ feature/ivindex/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    // https://github.com/NordicSemiconductor/Android-Gradle-Plugins/blob/main/plugins/src/main/kotlin/AndroidFeatureConventionPlugin.kt
+    alias(libs.plugins.nordic.feature)
+    alias(libs.plugins.kotlin.parcelize)
+}
+
+android {
+    namespace = "no.nordicsemi.android.nrfmesh.feature.ivindex"
+}
+
+dependencies {
+
+    implementation(libs.nordic.kotlin.data)
+    implementation(libs.kotlinx.datetime)
+
+    implementation(project(":core:data"))
+    implementation(project(":core:navigation"))
+    implementation(project(":core:ui"))
+    implementation(project(":mesh:core"))
+
+    implementation("androidx.compose.material:material-icons-extended-android:1.7.8")
+}
