ee400244a021b5668b131ae8c49fde253261f8d0
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2025-11-24 14:07:48 +0100

    Fix proguard rules

--- core/navigation/src/main/java/no/nordicsemi/android/nrfmesh/core/navigation/ClickableSetting.kt
+++ core/navigation/src/main/java/no/nordicsemi/android/nrfmesh/core/navigation/ClickableSetting.kt
@@ -1,18 +1,16 @@
 package no.nordicsemi.android.nrfmesh.core.navigation
 
-import androidx.annotation.Keep
 import kotlinx.serialization.Serializable
 
 /**
  * ClickableSetting is a sealed class that represents the clickable settings item in the settings
  * screen. This is used to highlight the selected item in the settings list ui
  */
-@Keep
 @Serializable
 enum class ClickableSetting {
     PROVISIONERS,
     NETWORK_KEYS,
     APPLICATION_KEYS,
     SCENES,
-    IV_INDEX,
+    IV_INDEX
 }
