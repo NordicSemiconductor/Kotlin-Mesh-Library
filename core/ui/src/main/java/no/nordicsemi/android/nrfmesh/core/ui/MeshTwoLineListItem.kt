03ba69c55f19b95f77839c8ce32e89c141cf1d7b
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2025-12-19 11:01:22 +0100

    Fix imports

--- core/ui/src/main/java/no/nordicsemi/android/nrfmesh/core/ui/MeshTwoLineListItem.kt
+++ core/ui/src/main/java/no/nordicsemi/android/nrfmesh/core/ui/MeshTwoLineListItem.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
-import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
