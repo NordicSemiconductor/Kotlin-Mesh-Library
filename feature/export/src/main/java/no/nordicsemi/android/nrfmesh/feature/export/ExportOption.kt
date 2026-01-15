78e8aaac982e86605ba5fd5d2f3795d0fde2bb90
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2026-01-15 10:27:11 +0100

    Add missing documentation

--- feature/export/src/main/java/no/nordicsemi/android/nrfmesh/feature/export/ExportOption.kt
+++ feature/export/src/main/java/no/nordicsemi/android/nrfmesh/feature/export/ExportOption.kt
@@ -1,6 +1,15 @@
 package no.nordicsemi.android.nrfmesh.feature.export
 
+/**
+ * Options for exporting mesh data.
+ */
 enum class ExportOption {
+    /**
+     * Exports all data.
+     */
     ALL,
+    /**
+     * Exports partial data.
+     */
     PARTIAL
 }
