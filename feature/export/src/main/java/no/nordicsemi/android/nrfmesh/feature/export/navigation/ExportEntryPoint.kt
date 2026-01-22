b280e08942d02c07833651813d8f2abd7a5c3180
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2026-01-22 10:23:05 +0100

    Migrate to flow

--- feature/export/src/main/java/no/nordicsemi/android/nrfmesh/feature/export/navigation/ExportEntryPoint.kt
+++ feature/export/src/main/java/no/nordicsemi/android/nrfmesh/feature/export/navigation/ExportEntryPoint.kt
@@ -1,15 +1,23 @@
 package no.nordicsemi.android.nrfmesh.feature.export.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.compose.runtime.getValue
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
 import no.nordicsemi.android.nrfmesh.feature.export.ExportRoute
 import no.nordicsemi.android.nrfmesh.feature.export.ExportViewModel
 
+@Serializable
+data object ExportKey : NavKey
+
 @Composable
-fun ExportScreenRoute(onDismissRequest: () -> Unit, onExportCompleted: (String) -> Unit, ) {
+fun ExportScreenRoute(onDismissRequest: () -> Unit, onExportCompleted: (String) -> Unit) {
     val viewModel = hiltViewModel<ExportViewModel>()
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
     ExportRoute(
-        uiState = viewModel.uiState,
+        uiState = uiState,
         onExportOptionSelected = viewModel::onExportOptionSelected,
         onNetworkKeySelected = viewModel::onNetworkKeySelected,
         onProvisionerSelected = viewModel::onProvisionerSelected,
