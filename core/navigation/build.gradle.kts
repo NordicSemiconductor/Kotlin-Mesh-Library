7def6495c116b4c9223d0e8602a495c2fb7cbc4d
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2026-01-22 11:17:54 +0100

    Fix gradle dependencies

--- core/navigation/build.gradle.kts
+++ core/navigation/build.gradle.kts
@@ -28,5 +28,4 @@ dependencies {
 
     implementation("androidx.compose.material:material-icons-extended-android:1.7.8")
     implementation("androidx.compose.material3:material3-window-size-class:1.4.0")
-    implementation("androidx.compose.material3.adaptive:adaptive-navigation:1.2.0")
 }
--- feature/settings/build.gradle.kts
+++ feature/settings/build.gradle.kts
@@ -10,9 +10,10 @@ android {
 }
 
 dependencies {
-
     implementation(libs.kotlinx.datetime)
     implementation(libs.kotlinx.serialization.json)
+
+    implementation(libs.androidx.compose.adaptive.navigation3)
 
     implementation(project(":core:ui"))
     implementation(project(":core:data"))
@@ -24,7 +25,6 @@ dependencies {
     implementation(project(":feature:scenes"))
     implementation(project(":feature:ivindex"))
     implementation(project(":mesh:core"))
-    implementation(libs.androidx.compose.adaptive.navigation3)
 
     testImplementation(libs.junit4)
     testImplementation(libs.kotlin.junit)
@@ -35,12 +35,4 @@ dependencies {
     androidTestImplementation(libs.kotlin.junit)
     androidTestImplementation(libs.androidx.test.ext)
     androidTestImplementation(libs.androidx.test.rules)
-
-    implementation("androidx.compose.material3:material3:1.5.0-alpha10")
-    implementation("androidx.compose.material3:material3-window-size-class:1.4.0")
-    implementation("androidx.compose.material3:material3-adaptive-navigation-suite:1.4.0")
-
-    implementation("androidx.compose.material3.adaptive:adaptive:1.2.0")
-    implementation("androidx.compose.material3.adaptive:adaptive-layout:1.2.0")
-    implementation("androidx.compose.material3.adaptive:adaptive-navigation-android:1.2.0")
 }
