0a956cdd135c9a6ceacccbefc8306381b1e60d64
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2026-02-09 13:33:46 +0100

    Fix imports

--- core/ui/src/main/java/no/nordicsemi/android/nrfmesh/core/ui/PlaceHolder.kt
+++ core/ui/src/main/java/no/nordicsemi/android/nrfmesh/core/ui/PlaceHolder.kt
@@ -1,26 +1,16 @@
 package no.nordicsemi.android.nrfmesh.core.ui
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.text.style.TextAlign
