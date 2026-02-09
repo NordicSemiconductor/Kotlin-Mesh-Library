bb43697556d3f07893865509d811c89d0cac40f1
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2026-02-09 13:31:57 +0100

    Use warning view from commons

--- core/ui/src/main/java/no/nordicsemi/android/nrfmesh/core/ui/PlaceHolder.kt
+++ core/ui/src/main/java/no/nordicsemi/android/nrfmesh/core/ui/PlaceHolder.kt
@@ -1,10 +1,14 @@
 package no.nordicsemi.android.nrfmesh.core.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -15,8 +19,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import no.nordicsemi.android.common.ui.view.WarningView
 
 
 @Composable
@@ -26,7 +35,7 @@ fun PlaceHolder(
     text: String,
 ) {
     OutlinedCard(
-        modifier = modifier.padding(top = 48.dp, end = 16.dp),
+        modifier = modifier.padding(top = 48.dp, end = 16.dp, bottom = 16.dp),
         colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface),
         shape = RoundedCornerShape(
             topStart = 16.dp,
@@ -35,47 +44,22 @@ fun PlaceHolder(
             bottomStart = 16.dp
         ),
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(horizontal = 16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(
-                20.dp,
-                alignment = Alignment.CenterVertically,
-            ),
+        Box(
+            contentAlignment = Alignment.Center
         ) {
-            Icon(
-                modifier = Modifier.size(120.dp),
-                imageVector = imageVector,
-                contentDescription = null,
-                tint = MaterialTheme.colorScheme.primary,
+            WarningView(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(all = 8.dp),
+                painterResource = rememberVectorPainter(imageVector),
+                title = {
+                    Text(
+                        text = text,
+                        textAlign = TextAlign.Center
+                    )
+                },
+                hint = {}
             )
-            Text(text = text)
-        }
-    }
-}
-
-@Composable
-fun PlaceHolder(modifier: Modifier = Modifier, content: @Composable () -> Unit) {
-    Card(
-        modifier = modifier.padding(top = 40.dp, end = 16.dp),
-        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface),
-        shape = RoundedCornerShape(
-            topStart = 16.dp,
-            topEnd = 16.dp,
-            bottomEnd = 16.dp,
-            bottomStart = 16.dp
-        ),
-    ) {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(
-                20.dp, alignment = Alignment.CenterVertically,
-            ),
-        ) {
-            content()
         }
     }
 }
