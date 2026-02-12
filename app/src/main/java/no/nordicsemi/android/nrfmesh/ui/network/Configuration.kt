a9d5d37702cc9d09993e635db12ef2f13f21f80b
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2026-02-12 08:56:25 +0100

    Defines a set of Configurations to be used by the network wizard

--- app/src/main/java/no/nordicsemi/android/nrfmesh/ui/network/Configuration.kt
+++ app/src/main/java/no/nordicsemi/android/nrfmesh/ui/network/Configuration.kt
@@ -9,49 +9,71 @@ import androidx.compose.material.icons.outlined.HourglassEmpty
 import androidx.compose.material.icons.outlined.ImportExport
 import androidx.compose.material.icons.outlined.VpnKey
 
-sealed class Configuration(
-    val networkKeys: Int = 0,
-    val applicationKeys: Int = 0,
-    val groups: Int = 0,
-    val virtualGroups: Int = 0,
-    val scenes: Int = 0,
-) {
-    data object Empty : Configuration(networkKeys = 1)
+/**
+ * Network properties
+ *
+ * @property networkKeys     Number of network keys.
+ * @property applicationKeys Number of application keys.
+ * @property groups          Number of groups.
+ * @property virtualGroups   Number of virtual groups.
+ * @property scenes          Number of scenes.
+ */
+interface NetworkProperties{
+    val networkKeys: Int
+    val applicationKeys: Int
+    val groups: Int
+    val virtualGroups: Int
+    val scenes: Int
+}
 
-    class  Custom(
-        networkKeys: Int = 1,
-        applicationKeys: Int = 0,
-        groups: Int = 3,
-        virtualGroups: Int = 1,
-        scenes: Int = 4,
-    ) : Configuration(
-        networkKeys = networkKeys,
-        applicationKeys = applicationKeys,
-        groups = groups,
-        virtualGroups = virtualGroups,
-        scenes = scenes
-    )
+/**
+ * Defines the network configuration used by the Network Wizard
+ */
+sealed class Configuration {
 
-    class Debug(
-        networkKeys: Int = 1,
-        applicationKeys: Int = 1,
-        groups: Int = 3,
-        virtualGroups: Int = 1,
-        scenes: Int = 4,
-    ) : Configuration(
-        networkKeys = networkKeys,
-        applicationKeys = applicationKeys,
-        groups = groups,
-        virtualGroups = virtualGroups,
-        scenes = scenes
-    )
+    /**
+     * Empty configuration.
+     */
+    data object Empty : Configuration(), NetworkProperties {
+        override val networkKeys = 0
+        override val applicationKeys = 0
+        override val groups = 0
+        override val virtualGroups = 0
+        override val scenes = 0
+    }
 
+    /**
+     * Custom configuration.
+     */
+    data class  Custom(
+        override val networkKeys: Int = 1,
+        override val applicationKeys: Int = 1,
+        override val groups: Int = 3,
+        override val virtualGroups: Int = 1,
+        override val scenes: Int = 4,
+    ) : Configuration(), NetworkProperties
+
+    /**
+     * Debug configuration.
+     */
+    data class Debug(
+        override val networkKeys: Int = 1,
+        override val applicationKeys: Int = 0,
+        override val groups: Int = 3,
+        override val virtualGroups: Int = 1,
+        override val scenes: Int = 4,
+    ) : Configuration(), NetworkProperties
+
+    /**
+     * Import configuration.
+     */
     data object Import : Configuration()
 }
 
-val configurations =
-    listOf(Configuration.Empty, Configuration.Custom(), Configuration.Debug(), Configuration.Import)
 
+/**
+ * Returns the icon for the configuration.
+ */
 internal fun Configuration.icon() = when (this) {
     is Configuration.Empty -> Icons.Outlined.HourglassEmpty
     is Configuration.Custom -> Icons.Outlined.DashboardCustomize
@@ -59,6 +81,9 @@ internal fun Configuration.icon() = when (this) {
     is Configuration.Import -> Icons.Outlined.ImportExport
 }
 
+/**
+ * Returns the description for the configuration.
+ */
 internal fun Configuration.description(): String {
     return when (this) {
         is Configuration.Empty -> "Empty"
@@ -68,6 +93,9 @@ internal fun Configuration.description(): String {
     }
 }
 
+/**
+ * Configuration property.
+ */
 enum class ConfigurationProperty {
     NETWORK_KEYS,
     APPLICATION_KEYS,
@@ -76,6 +104,9 @@ enum class ConfigurationProperty {
     SCENES
 }
 
+/**
+ * Returns the description for the configuration property.
+ */
 fun ConfigurationProperty.description(): String = when (this) {
     ConfigurationProperty.NETWORK_KEYS -> "Network Keys"
     ConfigurationProperty.APPLICATION_KEYS -> "Application Keys"
@@ -84,6 +115,9 @@ fun ConfigurationProperty.description(): String = when (this) {
     ConfigurationProperty.SCENES -> "Scenes"
 }
 
+/**
+ * Returns the icon for the configuration property.
+ */
 fun ConfigurationProperty.icon() = when (this) {
     ConfigurationProperty.NETWORK_KEYS,
     ConfigurationProperty.APPLICATION_KEYS,
@@ -96,7 +130,17 @@ fun ConfigurationProperty.icon() = when (this) {
     ConfigurationProperty.SCENES -> Icons.Outlined.AutoAwesome
 }
 
+/**
+ * Action
+ */
 enum class Action{
+    /**
+     * Add action.
+     */
     ADD,
+
+    /**
+     * Remove action.
+     */
     REMOVE
 }
