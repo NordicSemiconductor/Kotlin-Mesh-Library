83c4ed05962adeb9350efe61372f99dc25f57581
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2026-02-12 11:52:19 +0100

    Fix floating action button alignment

--- feature/nodes/src/main/java/no/nordicsemi/android/nrfmesh/feature/nodes/NodesScreen.kt
+++ feature/nodes/src/main/java/no/nordicsemi/android/nrfmesh/feature/nodes/NodesScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -69,7 +70,10 @@ private fun Nodes(
             navigateToNode = navigateToNode
         )
         ExtendedFloatingActionButton(
-            modifier = Modifier.defaultMinSize(minWidth = 150.dp),
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(16.dp)
+                .defaultMinSize(minWidth = 150.dp),
             text = { Text(text = stringResource(R.string.label_add_node)) },
             icon = {
                 Icon(
