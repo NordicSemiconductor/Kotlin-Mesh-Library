5e7f884587800096175c00461e8e4d7b1782fc3c
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2026-02-26 02:03:36 +0100

    Defines a common editable key composable that can be used with both Network and Application Keys

--- feature/network-keys/src/main/java/no/nordicsemi/android/nrfmesh/feature/network/keys/key/NetworkKeyScreen.kt
+++ feature/network-keys/src/main/java/no/nordicsemi/android/nrfmesh/feature/network/keys/key/NetworkKeyScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.material.icons.outlined.Badge
 import androidx.compose.material.icons.outlined.FormatListNumbered
 import androidx.compose.material.icons.outlined.LocalPolice
 import androidx.compose.material.icons.outlined.Update
-import androidx.compose.material.icons.outlined.VpnKey
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -29,19 +28,16 @@ import androidx.compose.ui.unit.dp
 import no.nordicsemi.android.nrfmesh.core.common.copyToClipboard
 import no.nordicsemi.android.nrfmesh.core.ui.ElevatedCardItem
 import no.nordicsemi.android.nrfmesh.core.ui.ElevatedCardItemTextField
+import no.nordicsemi.android.nrfmesh.core.ui.KeyRow
 import no.nordicsemi.android.nrfmesh.core.ui.NetworkKeyRow
 import no.nordicsemi.android.nrfmesh.core.ui.SectionTitle
 import no.nordicsemi.android.nrfmesh.feature.network.keys.R
-import no.nordicsemi.kotlin.data.toByteArray
 import no.nordicsemi.kotlin.mesh.core.model.Insecure
-import no.nordicsemi.kotlin.mesh.core.model.KeyDistribution
 import no.nordicsemi.kotlin.mesh.core.model.KeyIndex
 import no.nordicsemi.kotlin.mesh.core.model.KeyRefreshPhase
 import no.nordicsemi.kotlin.mesh.core.model.NetworkKey
-import no.nordicsemi.kotlin.mesh.core.model.NormalOperation
 import no.nordicsemi.kotlin.mesh.core.model.Secure
 import no.nordicsemi.kotlin.mesh.core.model.Security
-import no.nordicsemi.kotlin.mesh.core.model.UsingNewKeys
 import java.text.DateFormat
 import java.util.Date
 import kotlin.time.ExperimentalTime
@@ -93,6 +89,7 @@ private fun NetworkKeyContent(
         )
         Key(
             key = key.key,
+            isInUse = key.isInUse,
             onKeyChanged = {
                 key.setKey(it)
                 save()
@@ -130,6 +127,7 @@ private fun Name(
 @Composable
 private fun Key(
     key: ByteArray,
+    isInUse: Boolean,
     onKeyChanged: (ByteArray) -> Unit,
     isCurrentlyEditable: Boolean,
     onEditableStateChanged: () -> Unit,
@@ -137,10 +135,10 @@ private fun Key(
     val scope = rememberCoroutineScope()
     val context = LocalContext.current
     val clipboard = LocalClipboard.current
-    ElevatedCardItemTextField(
+    KeyRow(
         modifier = Modifier
             .padding(horizontal = 16.dp)
-            .clickable {
+            .clickable(enabled = isCurrentlyEditable) {
                 copyToClipboard(
                     scope = scope,
                     clipboard = clipboard,
@@ -148,13 +146,11 @@ private fun Key(
                     label = context.getString(R.string.label_network_key)
                 )
             },
-        imageVector = Icons.Outlined.VpnKey,
         title = stringResource(id = R.string.label_key),
-        subtitle = key.toHexString(format = HexFormat.UpperCase),
-        onValueChanged = { onKeyChanged(it.toByteArray()) },
-        isEditable = isCurrentlyEditable,
-        onEditableStateChanged = onEditableStateChanged,
-        regex = Regex("[0-9A-Fa-f]{32}")
+        key = key,
+        onKeyChanged = onKeyChanged,
+        isEditable = isCurrentlyEditable && !isInUse,
+        onEditableStateChanged = onEditableStateChanged
     )
 }
 
@@ -223,14 +219,6 @@ private fun LastModified(timestamp: Instant) {
             .getDateTimeInstance()
             .format(Date(timestamp.toEpochMilliseconds()))
     )
-}
-
-@Suppress("unused")
-@Composable
-private fun KeyRefreshPhase.description(): String = when (this) {
-    NormalOperation -> stringResource(id = R.string.label_normal_operation)
-    KeyDistribution -> stringResource(id = R.string.label_key_distribution)
-    UsingNewKeys -> stringResource(id = R.string.label_using_new_keys)
 }
 
 @Composable
