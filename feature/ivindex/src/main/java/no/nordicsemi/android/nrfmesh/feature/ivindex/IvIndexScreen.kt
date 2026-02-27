ea25b8476a51bda26b43e71f3c2ca93c7f0ed42a
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2026-02-27 11:16:46 +0100

    Clean up

--- feature/ivindex/src/main/java/no/nordicsemi/android/nrfmesh/feature/ivindex/IvIndexScreen.kt
+++ feature/ivindex/src/main/java/no/nordicsemi/android/nrfmesh/feature/ivindex/IvIndexScreen.kt
@@ -114,7 +114,7 @@ private fun IvIndex(
 
     if (showIvIndexDialog) {
         MeshAlertDialog(
-            onDismissRequest = { showIvIndexDialog = false },
+            onDismissRequest = { showIvIndexDialog = !showIvIndexDialog },
             onConfirmClick = {
                 if(ivIndexValue.text.isBlank()){
                     isError = true
@@ -126,9 +126,9 @@ private fun IvIndex(
                             ivIndexUpdateState
                         )
                     }.onSuccess {
-                        showIvIndexDialog = false
+                        showIvIndexDialog = !showIvIndexDialog
                     }.onFailure {
-                        isError = true
+                        isError = !isError
                         errorMessage = if (it is IvIndexTooSmall) {
                             context.getString(R.string.label_iv_index_too_small_error)
                         } else {
@@ -137,7 +137,7 @@ private fun IvIndex(
                     }
                 }
             },
-            onDismissClick = { showIvIndexDialog = false },
+            onDismissClick = { showIvIndexDialog = !showIvIndexDialog },
             icon = Icons.Outlined.FormatListNumbered,
             iconColor = AlertDialogDefaults.iconContentColor,
             title = stringResource(R.string.label_change_iv_index),
