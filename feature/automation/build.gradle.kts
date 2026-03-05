ecf01bba3ad2707f77b9bed76c70da0b3e1cfa04
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2026-03-05 09:27:48 +0100

    Adds automation module responsible for automatic configuration

---
+++ feature/automation/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    // https://github.com/NordicSemiconductor/Android-Gradle-Plugins/blob/main/plugins/src/main/kotlin/AndroidFeatureConventionPlugin.kt
+    alias(libs.plugins.nordic.feature)
+}
+
+android {
+    namespace = "no.nordicsemi.android.nrfmesh.feature.automation"
+}
+
+dependencies {
+    implementation(nordic.kotlin.data)
+
+    testImplementation(libs.junit4)
+    testImplementation(libs.kotlin.junit)
+    testImplementation(libs.androidx.test.ext)
+    testImplementation(libs.androidx.test.rules)
+
+    androidTestImplementation(libs.junit4)
+    androidTestImplementation(libs.kotlin.junit)
+    androidTestImplementation(libs.androidx.test.ext)
+    androidTestImplementation(libs.androidx.test.rules)
+
+    implementation(project(":core:common"))
+    implementation(project(":core:data"))
+    implementation(project(":core:ui"))
+    implementation(project(":mesh:core"))
+}
