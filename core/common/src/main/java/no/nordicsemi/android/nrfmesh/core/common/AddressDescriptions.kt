c9e37c23c6d1c5f21638e3094e43ea7502faf7e7
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2026-03-18 13:44:07 +0100

    Typos

--- core/common/src/main/java/no/nordicsemi/android/nrfmesh/core/common/AddressDescriptions.kt
+++ core/common/src/main/java/no/nordicsemi/android/nrfmesh/core/common/AddressDescriptions.kt
@@ -7,7 +7,7 @@ import no.nordicsemi.kotlin.mesh.core.model.AllRelays
 import no.nordicsemi.kotlin.mesh.core.model.FixedGroupAddress
 
 /**
- * Returns a human readable description of the [FixedGroupAddress].
+ * Returns a human-readable description of the [FixedGroupAddress].
  */
 fun FixedGroupAddress.name() = when (this) {
     is AllProxies -> "All Proxies"
